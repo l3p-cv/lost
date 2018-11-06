d07362a6845068db66052096dc900c0d9d9f66d5
Gereon Reus <gereon.reus@l3p-cv.com>
2018-11-06 14:58:06 +0100

    restructured api stuff in files

---
+++ backend/lost/api/user/endpoint.py
@@ -0,0 +1,73 @@
+import datetime
+from flask_restplus import Resource
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
+from lost.api.api import api
+from lost.api.user.api_definition import user, user_list, user_login
+from lost.api.user.parsers import login_parser
+from lost.settings import LOST_CONFIG, FLASK_DEBUG
+from lost.db import access, roles
+
+namespace = api.namespace('user', description='Users in System.')
+
+@namespace.route('/')
+class UserList(Resource):
+    @api.marshal_with(user_list)
+    @jwt_required 
+    def get(self):
+        dbm = access.DBMan(LOST_CONFIG)
+        identity = get_jwt_identity()
+        user = dbm.get_user_by_id(identity)
+        if not user.has_role(roles.DESIGNER):
+            dbm.close_session()
+            return "You are not authorized.", 401
+        else:
+            ulist = {'users':dbm.get_users()}
+            dbm.close_session()
+            return ulist
+
+
+@namespace.route('/<int:id>')
+@namespace.param('id', 'The user identifier')
+class User(Resource):
+    @api.marshal_with(user)
+    @jwt_required 
+    def get(self, id):
+        dbm = access.DBMan(LOST_CONFIG)
+        identity = get_jwt_identity()
+        user = dbm.get_user_by_id(identity)
+        if not user.has_role(roles.DESIGNER):
+            dbm.close_session()
+            return "You are not authorized.", 401
+
+        requesteduser = dbm.get_user_by_id(id)
+        dbm.close_session()
+        if requesteduser:
+            return requesteduser
+        else:
+            return "User with ID '{}' not found.".format(id)
+
+@namespace.route('/login/')
+class UserLogin(Resource):
+    @api.expect(user_login)
+    def post(self):
+        # get data from parser
+        data = login_parser.parse_args()
+        dbm = access.DBMan(LOST_CONFIG)
+        # find user in database
+        if 'email' in data:
+            user = dbm.find_user_by_email(data['email'])
+        if not user and 'user_name' in data:
+            user = dbm.find_user_by_user_name(data['user_name'])
+
+        # check password
+        if user and user.check_password(data['password']):
+            expires = datetime.timedelta(hours=2)
+            if FLASK_DEBUG:
+                expires = datetime.timedelta(days=365)
+            access_token = create_access_token(identity=user.idx, fresh=True, expires_delta=expires)
+            refresh_token = create_refresh_token(user.idx)
+            return {
+                'token': access_token,
+                'refresh_token': refresh_token
+            }, 200
+        return {'message': 'Invalid credentials'}, 401
--- backend/lost/app.py
+++ backend/lost/app.py
@@ -4,10 +4,9 @@ from flask_jwt_extended import JWTManager
 from lost import settings
 from lost.api.api import api
 
-from lost.api.user.endpoints.login import namespace as login_namespace
-from lost.api.user.endpoints.user import namespace as user_namespace
-from lost.api.group.endpoints.group import namespace as group_namespace
-from lost.api.sia.endpoints.sia import namespace as sia_namespace
+from lost.api.user.endpoint import namespace as user_namespace
+from lost.api.group.endpoint import namespace as group_namespace
+from lost.api.sia.endpoint import namespace as sia_namespace
 
 #from lost.database.db import db
 from lost.db.model import User, Role, UserRoles
@@ -51,7 +50,6 @@ def init_app(app):
     api.init_app(blueprint)
     #register endpoints here
     api.add_namespace(user_namespace)
-    api.add_namespace(login_namespace)
     api.add_namespace(group_namespace)
     api.add_namespace(sia_namespace)
     app.register_blueprint(blueprint)
