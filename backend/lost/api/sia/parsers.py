5974b17d95cccf619ad30e7d4160ee7cf4c33315
Gereon Reus <gereon.reus@l3p-cv.com>
2018-11-06 14:17:58 +0100

    hopefully finished sia api stuff

--- backend/lost/api/sia/parsers.py
+++
@@ -1,7 +0,0 @@
-from flask_restplus import reqparse
-
-sia_update_parser = reqparse.RequestParser()
-sia_update_parser.add_argument('data',
-                           type=str,
-                           required=True,
-                           help="Name of group.")
