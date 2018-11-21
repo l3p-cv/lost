245f3a871c334bc45b96ee6a549bb72f66609f1b
Gereon Reus <gereon.reus@l3p-cv.com>
2018-11-21 11:04:42 +0100

    first docker compose test try

--- docker/lost/pytest.sh
+++ docker/lost/pytest.sh
@@ -1,3 +1,8 @@
  #!/bin/bash
- export PYTHONPATH=$PYTHONPATH:/code/backend
- source /opt/conda/bin/activate lost && pytest /code/backend/lost
+export PYTHONPATH=$PYTHONPATH:/code/backend
+source /opt/conda/bin/activate lost
+
+mkdir -p ${LOST_HOME}/logs
+python3 /code/backend/lost/logic/init/initlost.py
+
+pytest /code/backend/lost
