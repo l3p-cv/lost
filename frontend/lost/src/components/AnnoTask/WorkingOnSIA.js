f2600e641fac839c4ff93cc5fd98eb2d042a54b4
Gereon Reus <gereon.reus@l3p-cv.com>
2018-11-23 13:51:00 +0100

    added working on sia task management

---
+++ frontend/lost/src/components/AnnoTask/WorkingOnSIA.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react'
+import {Progress} from 'reactstrap'
+import {getColor} from './utils'
+import {Col ,Row} from 'reactstrap'
+
+
+class WorkingOnSIA extends Component {
+    handleContinue(id){
+        console.log('Handle Continue OnClick, ID: ', id)
+    }
+    render() {
+        if(this.props.annoTask !== null){
+        let progress = Math.floor((this.props.annoTask.finished / this.props.annoTask.size) * 100)
+        return (
+            <div>
+                <Row>
+                <Col xs='4' md='4' xl='4'>
+                <div className='callout callout-danger'>
+                                <small className='text-muted'>Working on</small>
+                                <br/>
+                                <strong>{this.props.annoTask.name}</strong>
+                 </div>
+                </Col>
+                <Col xs='4' md='4' xl='4'>
+                  <div className='callout callout-warning'>
+                                <small className='text-muted'>Annotations</small>
+                                <br/>
+                                <strong className='h4'>{this.props.annoTask.finished}/{this.props.annoTask.size}</strong>
+                 </div>
+                </Col>
+                <Col xs='4' md='4' xl='4'>
+                  <div className='callout callout-success'>
+                                <small className='text-muted'>Seconds/Annotation</small>
+                                <br/>
+                                <strong className='h4'>&#8709; {this.props.annoTask.statistic.secondsPerAnno}</strong>
+                 </div>
+                </Col>
+                </Row>
+                <div className='clearfix'>
+                    <div className='float-left'>
+                        <strong>{progress}%</strong>
+                    </div>
+                    <div className='float-right'>
+                        <small className='text-muted'>Started at: {this.props.annoTask.createdAt}</small>
+                    </div>
+                </div>
+                <Progress className='progress-xs' color={getColor(progress)} value={progress}/>
+                <br/>
+            </div>
+        )
+    }else return(<React.Fragment><div>Loading...</div></React.Fragment>)
+    }
+}
+
+export default WorkingOnSIA
--- frontend/lost/src/views/SIA/SingleImageAnnotation.js
+++ frontend/lost/src/views/SIA/SingleImageAnnotation.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react"
+import {connect} from 'react-redux'
 import ReactDOM from "react-dom"
 
 import * as appView from "../../tools/sia/src/appView"
 import * as controlsView from "../../tools/sia/src/components/controls/controlsView"
 
 import LabelSelect from "../../components/LabelSelect/LabelSelect"
+import { 
+    CardHeader,
+    Card,
+    CardBody,
+    Col,
+    Row
+} from 'reactstrap'
+import WorkingOnSIA from '../../components/AnnoTask/WorkingOnSIA'
 
-export default class SingleImageAnnotation extends Component {
+
+import actions from '../../actions'
+const {getWorkingOnAnnoTask} = actions
+
+class SingleImageAnnotation extends Component {
 	constructor(props){
 		super(props)
 		this.mount = React.createRef()
 	}
 	componentDidMount(){
+		this.props.getWorkingOnAnnoTask()
 		require("../../tools/sia/src/appPresenter")
 		this.mount.current.appendChild(appView.html.fragment)
 		this.mount.current.appendChild(controlsView.html.fragment)
@@ -19,7 +33,30 @@ export default class SingleImageAnnotation extends Component {
 	}
 	render(){
 		return (
-			<div ref={this.mount} id="sia-mount"></div>
+			<Row>
+			<Col>
+				<Card>
+				<CardHeader>
+					Single Image Annotation
+				</CardHeader>
+					<CardBody>
+					<Row>
+						<Col  xs='12' sm='12' lg='12'>
+							<WorkingOnSIA annoTask={this.props.workingOnAnnoTask}></WorkingOnSIA>
+					<div ref={this.mount} id="sia-mount"></div>
+					</Col>
+					</Row>
+					  </CardBody> 
+				</Card>
+			</Col>
+		</Row>
+			
 		)
 	}
 }
+
+function mapStateToProps(state) {
+    return ({workingOnAnnoTask: state.annoTask.workingOnAnnoTask})
+}
+
+export default connect(mapStateToProps, {getWorkingOnAnnoTask})(SingleImageAnnotation)
