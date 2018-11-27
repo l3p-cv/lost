f414a9da22476101f1f89cd0bdc7e23f4c01cfe0
Jonas Jaeger <jonas.jaeger@et.hs-fulda.de>
2018-11-27 17:46:32 +0100

    Implemented test and dummy data creation for script api tests

--- backend/lost/utils/testils.py
+++ backend/lost/utils/testils.py
@@ -1,9 +1,10 @@
 '''Helper function for testing'''
 
-from lost.db import model
+from lost.db import model, dtype
 from lost.logic.label import LabelTree
 import datetime
 import pandas as pd
+from lost.logic.pipeline.instance import PipeInstance
 
 def get_user(dbm):
     email = 'test@example.com'
@@ -42,3 +43,66 @@ def get_voc_label_tree(dbm):
         name = df[df['parent_leaf_id'].isnull()]['name'].values[0]
         tree = LabelTree(dbm, name=name)
     return tree
+
+def get_script_pipeline_fragment(dbm):
+    '''Get a fragment of a pipeline
+
+    Script -> AnnoTask:
+        A Script connected to an AnnoTask
+
+    Returns:
+        :class:`lost.db.model.PipeElement`, :class:`lost.db.model.PipeElement`, :class:`lost.db.model.Pipe`: 
+            (script_element, annotation_task_element, pipeline)
+    '''
+    pipe = model.Pipe(name='TestPipe')
+    dbm.add(pipe)
+    dbm.commit()
+
+    script = model.Script(name='TestScript', path='data/pipes/test/test.py')
+    dbm.add(script)
+    dbm.commit()
+
+    pe_s = model.PipeElement(pipe_id=pipe.idx, dtype=dtype.PipeElement.SCRIPT)
+    pe_s.script = script
+    dbm.add(pe_s)
+    dbm.commit()
+
+    script_result = model.Result()
+    dbm.add(script_result)
+    dbm.commit()
+    
+    anno_task = model.AnnoTask(name='TestAnnoTask')
+    dbm.add(anno_task)
+
+    pe_a = model.PipeElement(pipe_id=pipe.idx, dtype=dtype.PipeElement.ANNO_TASK)
+    pe_a.anno_task = anno_task
+    # pe_a.result_in.append(script_result)
+    # pe_a.result_out.append(script_result)
+    dbm.add(pe_a)
+    dbm.commit()
+
+    # pe_s.pe_outs.append(pe_a)
+    # dbm.commit()
+
+    # Link elements
+    res_link_s = model.ResultLink(result_id=script_result.idx, 
+        pe_n=pe_s.idx, pe_out=pe_a.idx
+    )
+    res_link_a = model.ResultLink(result_id=script_result.idx, 
+        pe_n=pe_a.idx, pe_out=None
+    )
+    dbm.add(res_link_s)
+    dbm.add(res_link_a)
+    dbm.commit()
+
+    return pe_s, pe_a, pipe
+
+def delete_script_pipeline_fragment(dbm, pipe):
+    '''Delete a fragment of a pipeline
+
+    Script -> AnnoTask:
+        A Script connected to an AnnoTask
+    '''
+    # pi = PipeInstance(dbm, pipe)
+    # pi.delete_pipeline()
+    print('We should implement a working clean up here!')
