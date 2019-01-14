be05352b7f43f553ac0eca10c8b6d9024497e168
Gereon Reus <gereon.reus@l3p-cv.com>
2019-01-14 12:40:21 +0100

    merged

--- frontend/lost/src/containers/DefaultLayout/SubContainers/DefaultFooter.js
+++ frontend/lost/src/containers/DefaultLayout/SubContainers/DefaultFooter.js
@@ -15,7 +15,7 @@ class DefaultFooter extends Component {
 
     return (
       <React.Fragment>
-        <span><a href='https://l3p-cv.com'>LOST</a> &copy; 2018 L3P UG (haftungsbeschränkt).</span>
+        <span><a href='https://l3p-cv.com'>LOST</a> &copy; 2019 L3P UG (haftungsbeschränkt).</span>
         <span className='ml-auto'>Powered by <a href='https://l3p-cv.com'>L3P</a></span>
       </React.Fragment>
     )
