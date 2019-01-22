1ae9736c7d6d9e68b81cea867b9ba6e69064c3b5
Dennis Weiershäuser <dennis.weiershaeuser@informatik.hs-fulda.de>
2019-01-22 10:09:13 +0100

    update settings

--- frontend/lost/src/settings.js
+++ frontend/lost/src/settings.js
@@ -1,8 +1,15 @@
 // Production API URL - this url locates to nginx web server which is
 // hosting this app. Reverse Proxy to Flask API is configured in nginx configuration.
 // To make changes to the API URL, the reverse proxy configuration must be modified.
-export const API_URL = `${window.location.origin}/api`
+const DEV = false
 
-// Just for DEV - Adapt URL (Comment in) here if you are using npm dev server.
-//export const API_URL = `http://localhost/api`
+let API_URL = ""
+if(DEV){
+	console.warn('TURN OF DEV MODE IN PRODUCTION')
+	API_URL = `${window.location.origin.replace(/:\d+?$/, "")}/api`
+} else {
+	API_URL = `${window.location.origin}/api`
+}
+export { API_URL }
+
 export const roles = ['Designer', 'Annotater']
