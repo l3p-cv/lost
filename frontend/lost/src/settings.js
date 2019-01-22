7018f208c35690431bde0d9d4804dd45f9570840
Gereon Reus <gereon.reus@l3p-cv.com>
2019-01-22 08:46:49 +0100

    adapted API URL - ready for production: keep configuration in nginx reverse proxy, JS Devs needs to adapt this url in settings.js

--- frontend/lost/src/settings.js
+++ frontend/lost/src/settings.js
@@ -1,2 +1,8 @@
-export const API_URL = 'http://localhost/api'
+// Production API URL - this url locates to nginx web server which is
+// hosting this app. Reverse Proxy to Flask API is configured in nginx configuration.
+// To make changes to the API URL, the reverse proxy configuration must be modified.
+export const API_URL = `${window.location.origin}/api`
+
+// Just for DEV - Adapt URL (Comment in) here if you are using npm dev server.
+//export const API_URL = `http://localhost/api`
 export const roles = ['Designer', 'Annotater']
