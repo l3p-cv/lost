ba2900cbc710046771b705d3c534a065f8616eb8
Gereon Reus <gereon.reus@l3p-cv.com>
2019-01-24 14:39:10 +0100

    moved sphinx docs location and adapted build stuff

--- docker/lost/entrypoint.sh
+++ docker/lost/entrypoint.sh
@@ -46,7 +46,7 @@ mkdir -p ${LOST_HOME}/logs
 
 python3 /code/backend/lost/logic/init/initlost.py
 cd /code/backend/lost/cli && bash import_examples.sh && cd -
-cd /code/backend/lost/pyapi/docs &&  make html && cd -
+cd /code/docs/sphinx &&  make html && cd -
 
 if [ ${DEV} = "True" ]; then
   nginx="service nginx start"
--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.abspath('../../../../'))
+sys.path.append(os.path.abspath('../../backend/'))
 
 
 # -- Project information -----------------------------------------------------
