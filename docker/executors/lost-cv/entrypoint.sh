8c66431ed3b4db71287fbc784f7c148e75f3dd2a
Jonas Jaeger <jonas.jaeger@et.hs-fulda.de>
2019-01-29 16:43:09 +0100

    Fix of lost-cv entrypoint script

--- docker/executors/lost-cv/entrypoint.sh
+++ docker/executors/lost-cv/entrypoint.sh
@@ -2,6 +2,7 @@
  /bin/bash -c "source /opt/conda/bin/activate lost"
 source /opt/conda/bin/activate lost
 
+
  # init env vars 
 if [ -z "${LOST_DB_IP}" ]; then
   export LOST_DB_IP="db-lost"
@@ -30,22 +31,24 @@ while [ $n -ne 0 ]; do
 done
 
 #wait for rabbitmq
-# nc -z $RABBITMQ_IP $RABBITMQ_PORT
-# n=$?
-# while [ $n -ne 0 ]; do
-#     sleep 1
-#     nc -z $RABBITMQ_IP $RABBITMQ_PORT
-#     n=$?
-#     echo "$(date): Waiting for RabbitMQ@$RABBITMQ_IP:$RABBITMQ_PORT"
-# done
+nc -z $RABBITMQ_IP $RABBITMQ_PORT
+n=$?
+while [ $n -ne 0 ]; do
+    sleep 1
+    nc -z $RABBITMQ_IP $RABBITMQ_PORT
+    n=$?
+    echo "$(date): Waiting for RabbitMQ@$RABBITMQ_IP:$RABBITMQ_PORT"
+done
 
-mkdir -p ${LOST_HOME}/logs
+echo "All Services ready. Starting celery worker now."
+
+#mkdir -p ${LOST_HOME}/logs
 
 if [ ${DEV} = "True" ]; then
-  
+  echo "In DEV mode"
   #start a celery worker.
   worker="celery -A app.celery worker -l info --workdir /code/backend/lost/ -f ${LOST_HOME}/logs/lost-cv.log"
-  eval $worker &
+  eval $worker
 
 else
   echo "Production version not yet supported."
