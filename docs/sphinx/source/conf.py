b2b0710a5a4f669317f6b9159709df8c36a58d1f
Jonas Jaeger <jonas.jaeger@et.hs-fulda.de>
2019-03-06 17:22:42 +0100

    Adjusted pythonpath to python lost folder

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -20,7 +20,8 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.abspath('../../backend/'))
+# sys.path.append(os.path.abspath('../../backend/'))
+sys.path.append(os.path.abspath('../../../backend/'))
 
 
 # -- Project information -----------------------------------------------------
