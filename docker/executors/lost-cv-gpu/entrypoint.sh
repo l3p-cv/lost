38befe5e54cfe2df628f7943ede45bf6b697e0f0
Jonas Jaeger <jonas.jaeger@et.hs-fulda.de>
2019-03-22 01:16:29 +0100

    Working local example of gpu-worker

--- docker/executors/lost-cv-gpu/entrypoint.sh
+++ docker/executors/lost-cv-gpu/entrypoint.sh
@@ -2,12 +2,9 @@
  /bin/bash -c "source /opt/conda/bin/activate lost"
 source /opt/conda/bin/activate lost
 
-# clean celery lock
-rm -rf /tmp/celerybeat.pid
-
  # init env vars 
-
 export LOST_HOME="/home/lost"
+export PYTHONPATH=$PYTHONPATH:/code/backend
 
 if [ -z "${LOST_DB_IP}" ]; then
   export LOST_DB_IP="db-lost"
@@ -35,36 +32,22 @@ while [ $n -ne 0 ]; do
     echo "$(date): Waiting for MySQL@$LOST_DB_IP:$LOST_DB_PORT"
 done
 
-mkdir -p ${LOST_HOME}/logs
+#wait for rabbitmq
+nc -z $RABBITMQ_IP $RABBITMQ_PORT
+n=$?
+while [ $n -ne 0 ]; do
+    sleep 1
+    nc -z $RABBITMQ_IP $RABBITMQ_PORT
+    n=$?
+    echo "$(date): Waiting for RabbitMQ@$RABBITMQ_IP:$RABBITMQ_PORT"
+done
 
-python3 /code/backend/lost/logic/init/initlost.py
-cd /code/backend/lost/cli && bash import_examples.sh && cd -
-cd /code/docs/sphinx &&  make html && cd -
+echo "All Services ready. Starting celery worker now."
+
+#mkdir -p ${LOST_HOME}/logs
 python3 /code/backend/lost/logic/init/initworker.py
 
+#start celery worker.
+worker="celery -A flaskapp.celery worker -Q worker_status,$ENV_NAME -n $WORKER_NAME@%h -l info --workdir /code/backend/lost/ -f ${LOST_HOME}/logs/$WORKER_NAME.log"
+eval $worker
 
-# celery cronjob.
-celery="celery -A flaskapp.celery beat -l info --workdir /code/backend/lost/ -f ${LOST_HOME}/logs/beat.log  --schedule=/tmp/celerybeat-schedule --pidfile=/tmp/celerybeat.pid"
-eval $celery &
-
-#start a celery worker.
-worker="celery -A flaskapp.celery worker -Q celery,worker_status,$ENV_NAME -n $WORKER_NAME@%h -l info --workdir /code/backend/lost/ -f ${LOST_HOME}/logs/$WORKER_NAME.log"
-eval $worker &
-
-if [ ${DEBUG} = "True" ]; then
-  cp /code/docker/lost/nginx/dev.conf /etc/nginx/conf.d/default.conf
-  # start nginx web server
-  nginx="service nginx start"
-  eval $nginx &
-  # start flask dev server
-  endpoint="python3 /code/backend/lost/app.py"
-  eval $endpoint 
-else
-  cp /code/docker/lost/nginx/prod.conf /etc/nginx/conf.d/default.conf
-  # start nginx web server
-  nginx="service nginx start"
-  eval $nginx &
-  # start uswgi server
-  endpoint="cd /code/backend/lost/ && uwsgi --ini wsgi.ini --logto ${LOST_HOME}/logs/uswgi.log"
-  eval $endpoint 
-fi
