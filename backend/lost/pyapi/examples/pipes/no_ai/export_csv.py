fa7e8e88e64947e961ab7f576b54b08294b8c49c
Jonas Jaeger <jonas.jaeger@et.hs-fulda.de>
2019-03-22 16:31:52 +0100

    Adapted quick setup to allow more specific configurations

---
+++ backend/lost/pyapi/examples/pipes/no_ai/export_csv.py
@@ -0,0 +1,24 @@
+from lost.pyapi import script
+import os
+import pandas as pd
+
+ENVS = ['lost']
+ARGUMENTS = {'file_name' : { 'value':'annos.csv',
+                            'help': 'Name of the file with exported bbox annotations.'}
+            }
+
+class ExportCsv(script.Script):
+    '''This Script creates a csv file from image annotations and adds a data_export
+    to the output of this script in pipeline.
+    '''
+    def main(self):
+        df = self.inp.to_df()
+        csv_path = self.get_path(self.get_arg('file_name'), context='instance')
+        df.to_csv(path_or_buf=csv_path,
+                      sep=',',
+                      header=True,
+                      index=False)
+        self.outp.add_data_export(file_path=csv_path)
+
+if __name__ == "__main__":
+    my_script = ExportCsv()
