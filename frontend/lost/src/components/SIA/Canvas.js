8c39fd3a0e78a28a84bc4a5e14e536e99303c149
Jonas Jaeger <jonas.jaeger@et.hs-fulda.de>
2019-04-10 18:24:17 +0200

    First tries for zoom and camera feature on canvas

--- frontend/lost/src/components/SIA/Canvas.js
+++ frontend/lost/src/components/SIA/Canvas.js
@@ -4,6 +4,7 @@ import {connect} from 'react-redux'
 
 
 import actions from '../../actions'
+import './Canvas.scss';
 
 const { getSiaImage,getSiaAnnos } = actions
 
@@ -15,11 +16,42 @@ class Canvas extends Component{
             image: {
                 id: undefined,
                 data: undefined,
-            }
+            },
+            svg: {
+                width: '100%',
+                height: '100%',
+            },
+            svgTransfrom:''
         }
+        this.img = React.createRef()
+        this.svg = React.createRef()
     }
+    initCanvasView(){
+        var svgBox = this.svg.current.getBoundingClientRect()
+        var clientWidth = document.documentElement.clientWidth
+        var clientHeight = document.documentElement.clientHeight
+        var maxImgWidth = svgBox.right -svgBox.left
+        var maxImgHeight = clientHeight - svgBox.top
+        var ratio = this.img.current.naturalWidth / this.img.current.naturalHeight
+        var imgWidth = "100%"
+        var imgHeight = "100%"
+        console.log('naturalWidth', this.img.current.naturalWidth)
+        console.log('naturalHeight', this.img.current.naturalHeight)
+        if (maxImgHeight * ratio > maxImgWidth){
+            imgWidth = maxImgWidth
+            imgHeight = maxImgWidth / ratio
+        } else {
+            imgWidth = maxImgHeight * ratio
+            imgHeight = maxImgHeight
+        }
+        console.log('svg', this.svg)
+        console.log('img', this.img)
+        console.log('imgWidth, imgHeight', imgWidth, imgHeight)
+        this.setState({svg: {width : imgWidth, height: imgHeight}})
+    }
+
     componentDidMount(){
-        this.props.getSiaAnnos(50)
+        this.props.getSiaAnnos(11)
     }
 
     componentDidUpdate(){
@@ -28,34 +60,63 @@ class Canvas extends Component{
             if(this.props.annos.image.id !== this.state.image.id){
 			this.props.getSiaImage(this.props.annos.image.url).then(response=>
 			{
-			    this.setState({image: {...this.state.image, id: this.props.annos.image.id, data:window.URL.createObjectURL(response)}})}
-			)
+                this.setState({image: {...this.state.image, id: this.props.annos.image.id, data:window.URL.createObjectURL(response)}})
+            }
+            )       
+
         }
         }
+        // console.log('img', this.img)
+        // console.log('img width', this.img.current.width.baseVal.value)
+        // console.log('img height', this.img.current.height.baseVal.value)
+        // console.log('img getBBox', this.img.current.getBBox())
+        // console.log('img x, y', this.img.current.x.baseVal.value, this.img.current.y.baseVal.value)
+        // console.log('img.boundingClientRect', this.img.current.getBoundingClientRect())
+        // console.log('screen width, height', document.documentElement.clientWidth, document.documentElement.clientHeight) 
+
+
 	}
 
     renderDrawables(){
         if(this.props.annos.drawables){
-            this.props.annos.drawables.polygons.map((drawable) => {
-            console.log(drawable)
-          }
-       )}
+            console.log(this.props.annos.drawables)
+        //     this.props.annos.drawables.bboxes.map((drawable) => {
+        //     console.log(drawable)
+        //   })
+       }
        return(
             <g>
             <rect x="500" y="0" width="100" height="100" />
-            <circle onClick={(e)=>{console.log("JUUUHUUUUU",e.pageX)}}cx={520} cy={50} r={10} fill="red" />
+            <circle onClick={(e)=>{console.log("JUUUHUUUUU",e.pageX, e.pageY)}}cx={520} cy={50} r={10} fill="red" />
             <polygon points="100,100 150,25 150,75 200,0"
             fill="none" stroke="red" />
+            <line x1="0" y1="0" x2="200" y2="200" stroke='red'/>
             </g>
        )
     }
     render(){
         return(
                 <div>
-                  <svg width="100%" height='20cm'>
-                    <image href={this.state.image.data} width='70%'/>
+                  <svg ref={this.svg} width={this.state.svg.width} height={this.state.svg.height}>
+                    <g transform={this.state.svgTransform}>
+                    <image 
+                        
+                        onClick={(e) => {
+                            this.setState({svgTransform: "translate(-200) scale(2)"})
+                            console.log(this.state)
+                        }}
+                        onDoubleClick={(e) => {
+                            this.setState({svgTransform: ""})
+                            console.log(this.state)
+                        }}
+                        href={this.state.image.data} 
+                        width={this.state.svg.width} 
+                        height={this.state.svg.height}
+                         />
                     {this.renderDrawables()}
+                    </g>
                 </svg>
+                <img style={{display:'none'}} ref={this.img} onLoad={() => {this.initCanvasView()}} src={this.state.image.data} width="100%" height="100%"></img>
             </div>)
     }
 }
