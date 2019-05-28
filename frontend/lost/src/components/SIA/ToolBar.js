9ab56b368d0af6adcafc733070e6609d2e110110
Jonas Jaeger <jonas.jaeger@et.hs-fulda.de>
2019-05-28 14:33:52 +0200

    Added font awesome react component

--- frontend/lost/src/components/SIA/ToolBar.js
+++ frontend/lost/src/components/SIA/ToolBar.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import { Button } from 'reactstrap';
 import {connect} from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDrawPolygon, faVectorSquare, faWaveSquare, faDotCircle } from '@fortawesome/free-solid-svg-icons'
 import actions from '../../actions'
 import * as TOOLS from './types/tools'
 const { siaSelectTool } = actions
@@ -13,10 +15,18 @@ class ToolBar extends Component{
 
     render(){
         return(<div>
-                <Button onClick={e => this.onClick(e, TOOLS.POINT)} color="primary">Point</Button>{' '}
-                <Button onClick={e => this.onClick(e, TOOLS.LINE)} color="secondary">Line</Button>{' '}
-                <Button onClick={e => this.onClick(e, TOOLS.BBOX)} color="success">BBox</Button>{' '}
-                <Button onClick={e => this.onClick(e, TOOLS.POLYGON)} color="info">Polygon</Button>{' '}
+                <Button onClick={e => this.onClick(e, TOOLS.POINT)} color="primary">
+                    Point <FontAwesomeIcon icon={faDotCircle} />
+                </Button>{' '}
+                <Button onClick={e => this.onClick(e, TOOLS.LINE)} color="secondary">
+                    Line <FontAwesomeIcon icon={faWaveSquare} />
+                </Button>{' '}
+                <Button onClick={e => this.onClick(e, TOOLS.BBOX)} color="success">
+                    BBox <FontAwesomeIcon icon={faVectorSquare} size="3x"/>
+                </Button>{' '}
+                <Button onClick={e => this.onClick(e, TOOLS.POLYGON)} color="info">
+                    <FontAwesomeIcon icon={faDrawPolygon} size="3x"/>
+                </Button>{' '}
             </div>)
     }
 }
