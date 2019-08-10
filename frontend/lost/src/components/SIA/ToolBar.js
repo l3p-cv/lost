5af41bbe3303fef174b19536baa314c4e05184b9
Jonas Jaeger <jonas.jaeger@et.hs-fulda.de>
2019-08-10 17:29:11 +0200

    Added davinas icons to toolbar

--- frontend/lost/src/components/SIA/ToolBar.js
+++ frontend/lost/src/components/SIA/ToolBar.js
@@ -99,6 +99,71 @@ class ToolBar extends Component{
         this.props.siaShowImgBar(!this.props.imgBar.show)
     }
 
+    renderPointIcon(){
+        return (
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" 
+                // x="0px" y="0px"
+                // width="1190.549px" height="841.891px" 
+                viewBox="0 0 1190.549 841.891" 
+            >
+                <path fill="currentColor" d="M748.197,408.286c0,151.355-122.699,274.058-274.059,274.058c-151.357,0-274.057-122.703-274.057-274.058
+                    c0-151.356,122.7-274.057,274.057-274.057C625.497,134.229,748.197,256.929,748.197,408.286z"/>
+            </svg>
+        )
+    }
+
+    renderLineIcon(){
+        return (
+            <svg version="1.1" id="Linie" xmlns="http://www.w3.org/2000/svg"
+                // xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                // width="1190.549px" height="841.891px" 
+                viewBox="0 0 1190.549 841.891" 
+                // enable-background="new 0 0 1190.549 841.891"
+                // xml:space="preserve"
+                >
+                <path fill="currentColor" d="M986.331,109.582c7.141-10.669,28.926-51.179-2.968-85.299S891.011,8.972,891.011,8.972L539.125,133.474
+                    L63.239,302.022c-21.503,7.998-53.586,30.072-60.956,61.716s-2.116,58.538,22.414,96.79S211.33,752.213,228.564,777.28
+                    c26.453,36.868,62.16,58.042,105.507,57.154s365.665-7.485,365.665-7.485l331.7-8.325l89.453-2.293c0,0,70.998-7.179,69.861-60.287
+                    c-1.135-53.108-74.618-62.721-74.618-62.721s-733.215,15.553-749.148,15.576c-22.688,0.201-45.355-15.278-54.146-28.928
+                    s-175.79-272.956-175.79-272.956l600.587-212.338c0,0-73.179,67.087-99.315,100.041s-30.56,74.565-5.053,95.237
+                    c31.188,24.081,91.974-3.708,123.127-39.312C782.35,323.912,979.191,120.251,986.331,109.582z"/>
+            </svg>
+        )
+    }
+
+    renderBBoxIcon(){
+        return (
+            <svg version="1.1" id="Linie" xmlns="http://www.w3.org/2000/svg"
+                // xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                // width="1190.549px" height="841.891px" 
+                viewBox="0 0 1190.549 841.891" 
+                // enable-background="new 0 0 1190.549 841.891"
+                // xml:space="preserve"
+                >
+                <path fill="none" stroke="currentColor" stroke-width="120" stroke-miterlimit="10" d="M929.775,710.655
+                    c0,23.386-19.134,42.52-42.52,42.52H278.991c-23.386,0-42.52-19.134-42.52-42.52V102.392c0-23.386,19.134-42.52,42.52-42.52
+                    h608.264c23.386,0,42.52,19.134,42.52,42.52V710.655z"/>
+            </svg>
+        )
+    }
+
+    renderPolygonIcon(){
+        return (
+            <svg version="1.1" id="Linie" xmlns="http://www.w3.org/2000/svg"
+                // xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                // width="1190.549px" height="841.891px" 
+                viewBox="0 0 1190.549 841.891" 
+                // enable-background="new 0 0 1190.549 841.891"
+                // xml:space="preserve"
+                >
+                <path fill="none" stroke="currentColor" stroke-width="120" stroke-miterlimit="10" d="M342.327,769.938
+                    c-23.379,0.548-52.922-15.056-65.65-34.674L65.479,409.738c-12.729-19.619-5.085-41.998,16.984-49.732L917.331,67.421
+                    c22.07-7.734,26.86-0.275,10.645,16.576L691.761,329.475c-16.216,16.852-14.503,42.542,3.807,57.092l425.212,337.901
+                    c18.31,14.549,14.16,26.901-9.219,27.449L342.327,769.938z"/>
+            </svg>
+        )
+    }
+
     renderToolButtons(){
         if (!this.props.allowedActions.drawing) return null
         let btns = []
@@ -108,7 +173,8 @@ class ToolBar extends Component{
                     active={false} 
                     onClick={e => this.onClick(e, TOOLS.POINT)}
                 >
-                    <Icon name='dot circle' />
+                    {/* <Icon name='dot circle' /> */}
+                    {this.renderPointIcon()}
                 </Menu.Item>
                 // <Button key={TOOLS.POINT} outline onClick={e => this.onClick(e, TOOLS.POINT)} color="primary">
                 //     <FontAwesomeIcon icon={faDotCircle} size='1x' />
@@ -122,7 +188,8 @@ class ToolBar extends Component{
                     onClick={e => this.onClick(e, TOOLS.LINE)}
                 >
                     {/* <Icon name='paint brush' /> */}
-                    <FontAwesomeIcon icon={faWaveSquare} size="1x"/>
+                    {/* <FontAwesomeIcon icon={faWaveSquare} size="1x"/> */}
+                    {this.renderLineIcon()}
                 </Menu.Item>
                 // <Button key={TOOLS.LINE} outline onClick={e => this.onClick(e, TOOLS.LINE)} color="secondary">
                 //     <FontAwesomeIcon icon={faWaveSquare} size="1x"/>
@@ -135,7 +202,8 @@ class ToolBar extends Component{
                     active={false} 
                     onClick={e => this.onClick(e, TOOLS.BBOX)}
                 >
-                    <Icon name='square outline' />
+                    {/* <Icon name='square outline' /> */}
+                    {this.renderBBoxIcon()}
                 </Menu.Item>
                 // <Button key={TOOLS.BBOX} outline onClick={e => this.onClick(e, TOOLS.BBOX)} color="success">
                 //     <FontAwesomeIcon icon={faVectorSquare} size="1x"/>
@@ -149,7 +217,8 @@ class ToolBar extends Component{
                     onClick={e => this.onClick(e, TOOLS.POLYGON)}
                 >
                     {/* <Icon name='pencil alternate' /> */}
-                    <FontAwesomeIcon icon={faDrawPolygon} size="1x"/>
+                    {/* <FontAwesomeIcon icon={faDrawPolygon} size="1x"/> */}
+                    {this.renderPolygonIcon()}
                 </Menu.Item>
                 // <Button key={TOOLS.POLYGON} outline onClick={e => this.onClick(e, TOOLS.POLYGON)} color="info">
                 //     <FontAwesomeIcon icon={faDrawPolygon} size="1x"/>
