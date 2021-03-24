ad80287e0ea47e043ac4d4818da5cb86248bf3c0
Jonas Jäger <j.jaeger@l3bm.com>
2021-03-24 16:49:36 +0100

    Created test pipeline for mia annoBased filesystem abstraction stuff

---
+++ backend/lost/pyapi/examples/pipes/two_stage/request_annos.py
@@ -0,0 +1,47 @@
+from lost.pyapi import script
+import os
+import random
+
+ENVS = ['lost']
+ARGUMENTS = {'polygon' : { 'value':'false',
+                            'help': 'Add a dummy polygon proposal as example.'},
+            'line' : { 'value':'false',
+                            'help': 'Add a dummy line proposal as example.'},
+            'point' : { 'value':'false',
+                            'help': 'Add a dummy point proposal as example.'},
+            'bbox' : { 'value':'false',
+                            'help': 'Add a dummy bbox proposal as example.'}
+            }
+class RequestAnnos(script.Script):
+    '''Request annotations for each image of an imageset.
+
+    An imageset is basicly a folder with images.
+    '''
+    def main(self):
+        for ds in self.inp.datasources:
+            media_path = ds.path
+            annos = []
+            anno_types = []
+            if self.get_arg('polygon').lower() == 'true':
+                polygon= [[0.1,0.1],[0.4,0.1],[0.2,0.3]]
+                annos.append(polygon)
+                anno_types.append('polygon')
+            if self.get_arg('line').lower() == 'true':
+                line= [[0.5,0.5],[0.7,0.7]]
+                annos.append(line)
+                anno_types.append('line')
+            if self.get_arg('point').lower() == 'true':
+                point= [0.8,0.1]
+                annos.append(point)
+                anno_types.append('point')
+            if self.get_arg('bbox').lower() == 'true':
+                box= [0.6,0.6,0.1,0.05]
+                annos.append(box)
+                anno_types.append('bbox')
+            for img_file in os.listdir(media_path):
+                img_path = os.path.join(media_path, img_file)
+                self.outp.request_annos(img_path=img_path, annos=annos, anno_types=anno_types)
+                self.logger.info('Requested annos for: {}'.format(img_path))
+
+if __name__ == "__main__":
+    my_script = RequestAnnos() 
