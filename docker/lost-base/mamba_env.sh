9918643b1a28da5a10de0e068654ea3750fef865
Jonas Jäger <j.jaeger@l3bm.com>
2021-07-06 17:07:35 +0200

    lost-base add missing packages

--- docker/lost-base/mamba_env.sh
+++ docker/lost-base/mamba_env.sh
@@ -1,1 +1,1 @@
-mamba create -n lost imagesize mysqlclient pandas numpy pytest adlfs dask distributed bokeh flask flask-jwt-extended flask-ldap3-login flask-mail flask-restx flask-sqlalchemy flask-user flask-wtf fsspec opencv pudb python-igraph sphinx sk-video sqlalchemy 
+mamba create -n lost imagesize mysqlclient pandas numpy pytest adlfs dask distributed bokeh flask flask-jwt-extended flask-ldap3-login flask-mail flask-restx flask-sqlalchemy flask-user flask-wtf fsspec opencv pudb python-igraph sphinx sk-video sqlalchemy flask-cors flask-jwt-extended==3.13.1 scikit-image
