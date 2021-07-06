cd8d2c99d36ebceddfdd482290f5653ca30950b6
Jonas Jäger <j.jaeger@l3bm.com>
2021-07-06 14:13:34 +0200

    Use correct env for pytest

--- docker/lost/pytest.sh
+++ docker/lost/pytest.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:/code/backend
-source /opt/conda/bin/activate lost
+source /opt/mambaforge/bin/activate lost
 
 mkdir -p ${LOST_HOME}/logs
 python3 /code/backend/lost/logic/init/initlost.py
