3647028db7efea4b384c921f6d3566ae555cd3f7
Jonas Jäger <j.jaeger@l3bm.com>
2021-07-22 09:01:08 +0200

    Define config via environment variables

--- backend/lostconfig.py
+++ backend/lostconfig.py
@@ -1,138 +1,144 @@
-import os
-import ast
+from ast import literal_eval
+import subprocess
+import logging
+from os import environ as env
+from time import sleep
+import importlib
+from dask.distributed import Client, LocalCluster
+# from dask_yarn import YarnCluster
+
+def keytab_renewer_loop(logger_name):
+    def kinit(logger, local_keytab_path, principal):
+        try:
+            logger.debug(f"performing kinit -kt {local_keytab_path} {principal}")
+            p = subprocess.run([ 'kinit', '-kt', local_keytab_path, principal], capture_output=True)
+            logger.debug(f"stdout of subprocess: {str(p.stdout)}")
+        except:
+            logger.error(f"stderr of subprocess: {str(p.stderr)}")
+    logger = logging.getLogger('{}.{}'.format(logger_name,'keytab_renewer_loop'))
+    keytab = eval_env('LOST_KINIT_KEYTAB')
+    principal = eval_env('LOST_KINIT_PRINCIPAL')
+    sleep_time = ge('LOST_KINIT_SLEEP_TIME',60*60*6) # Sleep for 6 hours
+    local_keytab_path = f"{env['HOME']}/.{keytab.split('/')[-1]}"
+    while True:
+        kinit(logger, local_keytab_path, principal)
+        logger.info('Sleep for {} seconds'.format(sleep_time))
+        sleep(sleep_time)
+
+def eval_env(name):
+    try:
+        res = literal_eval(env[name])
+    except:
+        res = literal_eval(f"'{env[name]}'")
+    return res
+
+def ge(name, default):
+    '''Get env entry or default
+
+    Args:
+        name (str): Name of env variable
+        default (*): Default value that will be used if no env variable exists.
+    '''
+    if name in env:
+        return eval_env(name)
+    else:
+        return default
+
+def import_by_string(full_name):
+    module_name, unit_name = full_name.rsplit('.', 1)
+    mod = importlib.import_module(module_name)
+    return getattr(mod, unit_name)
+ 
 class LOSTConfig(object):
 
     def __init__(self):
 
-        self.debug = False
-        if "DEBUG" in os.environ:
-            if os.environ['DEBUG'].lower() == 'true':
-                self.debug = True
+        self.use_docker = ge('LOST_USE_DOCKER', True)
+        self.debug = ge('LOST_DEBUG_MODE', True)
+
+        # Storage for application related data
         self.app_path = '/home/lost/app'
-        self.data_fs_type = os.environ['LOST_DATA_FS_TYPE']
-        if self.data_fs_type.lower() == 'file':
-            self.data_path = '/home/lost/data'
-        else:
-            self.data_path = os.environ['LOST_DATA']
-        # print(os.environ['LOST_FS_ARGS'])
-        self.data_fs_args = ast.literal_eval(os.environ['LOST_DATA_FS_ARGS'])
-        self.py3_init = os.environ['PY3_INIT']
-        self.lost_db_user = os.environ['LOST_DB_USER']
-        self.lost_db_name = os.environ['LOST_DB_NAME']
-        self.lost_db_pwd = os.environ['LOST_DB_PASSWORD']
-        self.db_connector = "mysql+mysqldb"
+        if not self.use_docker:
+            self.app_path = ge('LOST_APP_PATH',self.app_path)
 
-        # Name of the environment that is installed in this container and used to execute scripts
-        self.env_name = os.environ['ENV_NAME']
+        # Storage for user/pipeline related data
+        self.data_fs_type = ge('LOST_DATA_FS_TYPE', 'file')
+        
+        self.data_path = '/home/lost/data'
+        if not self.use_docker:
+            self.data_path = ge('LOST_DATA_PATH', self.data_path)
+        self.data_fs_args = ge('LOST_DATA_FS_ARGS',{})
+
+        self.py3_init = ge('LOST_PY3_INIT','')
+
+        # Database access
+        self.lost_db_user = ge('LOST_DB_USER','lost')
+        self.lost_db_name = ge('LOST_DB_NAME','lost')
+        self.lost_db_pwd = ge('LOST_DB_PASSWORD','LostDevLost')
+        self.lost_db_port = str(ge('LOST_DB_PORT','3306'))
+        self.lost_db_ip = str(ge('LOST_DB_IP',"db-lost"))
+        self.db_connector = ge('LOST_DB_CONNECTOR',"mysql+mysqldb")
+
+        # Worker management
         # Unique name for this container worker
-        self.worker_name = os.environ['WORKER_NAME']
-        # Timeout when a worker is considered to be dead
-        self.worker_timeout = os.environ['WORKER_TIMEOUT']
-        # Intervall in seconds in which a worker should give a lifesign
-        self.worker_beat = int(os.environ['WORKER_BEAT'])
-        self.pipe_schedule = int(os.environ['PIPE_SCHEDULE'])
-        self.session_timeout = 30*60
-        self.scheduler_ip = 'localhost'
-        self.scheduler_port = 8786
+        self.worker_name = ge('LOST_WORKER_NAME','lost')
+        # Name of the environment that is installed in this container and used to execute scripts
+        self.env_name = self.worker_name
+        # Can be static or dynamic
+        self.worker_management = ge('LOST_WORKER_MANAGEMENT','static')
 
-        self.worker_management = 'static'
-        #Can be any attribute from model.User that identifies a unique user
-        self.dask_user_key = 'idx'
-        self.dask_spawn_as_proxy_user = False
+        if self.worker_management == 'dynamic':
+            # Yarn jobs
+            # self.extra_cron_jobs = [keytab_renewer_loop]
+            # from dask.distributed import Client, LocalCluster
+            # self.DaskCluster = LocalCluster
+            self.DaskCluster = import_by_string(
+                ge('LOST_DASK_CLUSTER','dask.distributed.LocalCluster'))
+            self.cluster_arguments = {'n_workers':1, 'processes':False}
+            self.DaskClient = Client
+            #Can be any attribute from model.User that identifies a unique user
+            self.dask_user_key = ge('LOST_DASK_USER_KEY','idx')
+            self.dask_spawn_as_proxy_user = ge('LOST_DASK_SPAWN_AS_PROXY_USER', False)
+        else:
+            # DASK scheduler properties
+            self.scheduler_ip = ge('LOST_SCHEDULER_IP','localhost')
+            self.scheduler_port = ge('LOST_SCHEDULER_PORT',8786)
         self.extra_cron_jobs = []
-        self.DaskCluster = None
-        self.cluster_arguments = {}
-        self.DaskClient = None
+        self.kinit_active = ge('LOST_KINIT_ACTIVE', False)
+        if self.kinit_active:
+            self.extra_cron_jobs.append(keytab_renewer_loop)
 
-        # DASK scheduler properties
-        self.scheduler_ip = 'localhost'
-        self.scheduler_port = 8786
-        
-        # self.session_timeout = 1
-        if "SESSION_TIMEOUT" in os.environ:
-            self.session_timeout = int(os.environ['SESSION_TIMEOUT'])
-            if self.session_timeout < 10:
-                self.session_timeout = 10
+        # Cron job stuff
+        # Timeout when a worker is considered to be dead
+        self.worker_timeout = ge('LOST_WORKER_TIMEOUT',30)
+        # Intervall in seconds in which a worker should give a lifesign
+        self.worker_beat = ge('LOST_WORKER_BEAT',10)
+        self.pipe_schedule = ge('LOST_PIPE_SCHEDULE',5)
+        self.session_timeout = ge('LOST_SESSION_TIME_OUT',30*60)
 
-        self.lost_db_port = '3306'
-        if "LOST_DB_PORT" in os.environ:
-            self.lost_db_port = os.environ['LOST_DB_PORT']
-        self.lost_db_ip = "db-lost"
-        if "LOST_DB_IP" in os.environ:
-            self.lost_db_ip = os.environ['LOST_DB_IP']
+        # Timout when a user get automatically logged out
+        self.session_timeout = ge('LOST_SESSION_TIMEOUT',30*60)
 
-        self.rabbitmq_port = '5672'
-        if "RABBITMQ_PORT" in os.environ:
-            self.rabbitmq_port = os.environ['RABBITMQ_PORT']
-        self.rabbitmq_ip = "rabbitmqlost"
-        if "RABBITMQ_IP" in os.environ:
-            self.rabbitmq_ip = os.environ['RABBITMQ_IP']
-
-        self.send_mail = False
-        self.mail_server = ""
-        self.mail_port = ""
-        self.mail_use_ssl = False
-        self.mail_use_tls = False
-        self.mail_username = ""
-        self.mail_password = ""
-        self.mail_default_sender = ""
-        self.mail_lost_url = ""
-            
-        if 'MAIL_SERVER' in os.environ:
-            self.mail_server = os.environ['MAIL_SERVER']
-            self.send_mail = True
-        if 'MAIL_PORT' in os.environ:
-            self.mail_port = os.environ['MAIL_PORT']
-        if 'MAIL_USE_SSL' in os.environ:
-            if os.environ['MAIL_USE_SSL'] == 'True':
-                self.mail_use_ssl = True
-        if 'MAIL_USE_TLS' in os.environ:
-            if os.environ['MAIL_USE_TLS'] == 'True':
-                self.mail_use_tls = True
-        if 'MAIL_USERNAME' in os.environ:
-            self.mail_username = os.environ['MAIL_USERNAME']
-        if 'MAIL_PASSWORD' in os.environ:
-            self.mail_password = os.environ['MAIL_PASSWORD']
-        if 'MAIL_DEFAULT_SENDER' in os.environ:
-            self.mail_default_sender = os.environ['MAIL_DEFAULT_SENDER']
-        if 'MAIL_LOST_URL' in os.environ:
-            self.mail_lost_url = os.environ['MAIL_LOST_URL']
-
+        # LDAP Configuation
         self.ldap_config = dict()
-        self.ldap_config['LDAP_ACTIVE'] = False
-        self.ldap_config['LDAP_PORT'] = 389
-        self.ldap_config['LDAP_GROUP_OBJECT_FILTER'] = '(objectclass=posixGroup)'
-        self.ldap_config['LDAP_GROUP_DN'] = ''
-        self.ldap_config['LDAP_USER_RDN_ATTR'] = 'cn'
-        self.ldap_config['LDAP_USER_LOGIN_ATTR'] = 'uid'
-        self.ldap_config['LDAP_USE_SSL'] = False
-        self.ldap_config['LDAP_ADD_SERVER'] = True
-        
-        if 'LDAP_ACTIVE' in os.environ:
-            self.ldap_config['LDAP_ACTIVE'] = os.environ['LDAP_ACTIVE']
-        if 'LDAP_HOST' in os.environ:
-            self.ldap_config['LDAP_HOST'] = os.environ['LDAP_HOST']
-        if 'LDAP_PORT' in os.environ:
-            self.ldap_config['LDAP_PORT'] = int(os.environ['LDAP_PORT'])
-        if 'LDAP_GROUP_OBJECT_FILTER' in os.environ:
-            self.ldap_config['LDAP_GROUP_OBJECT_FILTER'] = os.environ['LDAP_GROUP_OBJECT_FILTER']
-        if 'LDAP_BASE_DN' in os.environ:
-            self.ldap_config['LDAP_BASE_DN'] = os.environ['LDAP_BASE_DN']
-        if 'LDAP_USER_DN' in os.environ:
-            self.ldap_config['LDAP_USER_DN'] = os.environ['LDAP_USER_DN']
-        if 'LDAP_GROUP_DN' in os.environ:
-            self.ldap_config['LDAP_GROUP_DN'] = os.environ['LDAP_GROUP_DN']
-        if 'LDAP_USER_RDN_ATTR' in os.environ:
-            self.ldap_config['LDAP_USER_RDN_ATTR'] = os.environ['LDAP_USER_RDN_ATTR']
-        if 'LDAP_USER_LOGIN_ATTR' in os.environ:
-            self.ldap_config['LDAP_USER_LOGIN_ATTR'] = os.environ['LDAP_USER_LOGIN_ATTR']
-        if 'LDAP_BIND_USER_DN' in os.environ:
-            self.ldap_config['LDAP_BIND_USER_DN'] = os.environ['LDAP_BIND_USER_DN']
-        if 'LDAP_BIND_USER_PASSWORD' in os.environ:
-            self.ldap_config['LDAP_BIND_USER_PASSWORD'] = os.environ['LDAP_BIND_USER_PASSWORD' ]
-        if 'LDAP_USE_SSL' in os.environ:
-            self.ldap_config['LDAP_USE_SSL'] = os.environ['LDAP_USE_SSL']
-        if 'LDAP_ADD_SERVER' in os.environ:
-            self.ldap_config['LDAP_ADD_SERVER'] = os.environ['LDAP_ADD_SERVER']
+        self.ldap_config['LDAP_ACTIVE'] = ge('LOST_LDAP_ACTIVE', False)
+        self.ldap_config['LDAP_PORT'] = ge('LOST_LDAP_PROT', 389)
+        self.ldap_config['LDAP_GROUP_OBJECT_FILTER'] = ge(
+            'LOST_LDAP_GROUP_OBJECT_FILTER', '(objectclass=posixGroup)'
+        )
+        self.ldap_config['LDAP_GROUP_DN'] = ge('LOST_LDAP_GROUP_DN', '')
+        self.ldap_config['LDAP_USER_RDN_ATTR'] = ge('LOST_LDAP_USER_RDN_ATTR', 'cn')
+        self.ldap_config['LDAP_USER_LOGIN_ATTR'] = ge('LOST_LDAP_USER_LOGIN_ATTR', 'uid')
+        self.ldap_config['LDAP_USE_SSL'] = ge('LOST_LDAP_USE_SSL', False)
+        self.ldap_config['LDAP_ADD_SERVER'] = ge('LOST_LDAP_ADD_SERVER', True)
 
-        
+        # Mail configuration
+        self.send_mail = ge('LOST_SEND_MAIL', False)
+        self.mail_server = ge('LOST_MAIL_SEVER', "")
+        self.mail_port = ge('LOST_MAIL_PORT', "")
+        self.mail_use_ssl = ge('LOST_MAIL_USE_SSL', False)
+        self.mail_use_tls = ge('LOST_MAIL_USE_TLS', False)
+        self.mail_username = ge('LOST_MAIL_USERNAME', "")
+        self.mail_password = ge('LOST_MAIL_PASSWORD', "")
+        self.mail_default_sender = ge('LOST_MAIL_DEFAULT_SENDER', "")
+        self.mail_lost_url = ge('LOST_MAIL_LOST_URL', "")
