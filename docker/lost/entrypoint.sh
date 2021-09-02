cfc9b631dec050003bd7f5cc554a9df37392ce85
JonasGoebel <l3bm@jonasgoebel.net>
2021-09-02 14:26:39 +0200

    docker image is now using install.sh

--- docker/lost/entrypoint.sh
+++ docker/lost/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
  /bin/bash -c "source /opt/mambaforge/bin/activate lost"
 source /opt/mambaforge/bin/activate lost
+source /code/lost-env.sh
 
 # clean celery lock
 rm -rf /tmp/celerybeat.pid
@@ -36,11 +37,11 @@ done
 
 mkdir -p ${LOST_HOME}/logs
 
-python3 /code/backend/lost/logic/init/initlost.py
-cd /code/backend/lost/cli && bash import_examples.sh && cd -
+python3 /code/src/backend/lost/logic/init/initlost.py
+cd /code/src/backend/lost/cli && bash import_examples.sh && cd -
 # cd /code/docs/sphinx &&  make html && cd -
-python3 /code/backend/lost/logic/init/initworker.py
-python3 /code/backend/lost/logic/init/init_patchsystem.py
+python3 /code/src/backend/lost/logic/init/initworker.py
+python3 /code/src/backend/lost/logic/init/init_patchsystem.py
 
 
 # start scheduler.
@@ -51,27 +52,33 @@ eval $daskscheduler &
 worker="dask-worker localhost:8786 --name $ENV_NAME"
 eval $worker &
 
-cron_jobs="python3 /code/backend/lost/logic/jobs/cron_jobs.py"
+cron_jobs="python3 /code/src/backend/lost/logic/jobs/cron_jobs.py"
 eval $cron_jobs &
+
+# remove config in case debug mode changed
+if [ -f "/etc/nginx/conf.d/lost.conf" ]; then
+  rm /etc/nginx/conf.d/lost.conf
+fi
 
 if [ ${LOST_DEBUG_MODE} = "True" ]; then
   if [ "$CUSTOM_NGINX_CONF" != "True" ]; then
-	cp /code/docker/lost/nginx/dev.conf /etc/nginx/conf.d/default.conf
+    ln -s /etc/nginx/sites-available/dev.conf /etc/nginx/conf.d/lost.conf
   fi
   # start nginx web server
   nginx="service nginx start"
   eval $nginx &
   # start flask dev server
-  endpoint="python3 /code/backend/lost/app.py"
+  endpoint="python3 /code/src/backend/lost/app.py"
   eval $endpoint 
 else
   if [ "$CUSTOM_NGINX_CONF" != "True" ]; then
-	cp /code/docker/lost/nginx/prod.conf /etc/nginx/conf.d/default.conf
+	  ln -s /etc/nginx/sites-available/prod.conf /etc/nginx/conf.d/lost.conf
   fi
   # start nginx web server
   nginx="service nginx start"
   eval $nginx &
   # start uswgi server
-  endpoint="cd /code/backend/lost/ && uwsgi --ini wsgi.ini --logto ${LOST_HOME}/logs/uswgi.log"
+  #endpoint="cd /code/src/backend/lost/ && uwsgi --ini wsgi.ini --logto ${LOST_HOME}/logs/uswgi.log"
+  endpoint="cd /code/ && bash start.sh"
   eval $endpoint 
 fi
