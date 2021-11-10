5a300d8dc2259fa4e6c7503aeb5b442912d696c6
jgoebel <l3bm@jonasgoebel.net>
2021-11-10 11:00:47 +0100

    applied LOST design to charts

--- frontend/lost/src/containers/DesignerDashboard/DesignerDashboard.jsx
+++ frontend/lost/src/containers/DesignerDashboard/DesignerDashboard.jsx
@@ -35,18 +35,26 @@ const DesignerDashboard = () => {
         return(<div>Loading...</div>)
     }
 
+    const chartColors = {
+        background: 'rgb(16, 81, 95,0.8)',
+        borderColor: 'rgb(16, 81, 95,0.8)',
+        borderWidth: 0,
+        hoverBackgroundColor:'rgb(16, 81, 95,0.4)',
+        hoverBorderColor: 'twitter'
+    }
+
     const annoLabels = {
         labels: ['Cat', 'Dog', 'Monkey', 'Giraffe', 'Gereon', 'Lizard', 'Lama'],
         datasets: [
             {
                 label: 'Annotated Labels',
-                backgroundColor: 'rgba(255,99,132,0.2)',
-                borderColor: 'rgba(255,99,132,1)',
-                borderWidth: 0,
-                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                hoverBorderColor: 'rgba(255,99,132,1)',
+                backgroundColor: chartColors.background,
+                borderColor: chartColors.borderColor,
+                borderWidth: chartColors.borderWidth,
+                hoverBackgroundColor: chartColors.hoverBackgroundColor,
+                hoverBorderColor: chartColors.hoverBorderColor,
                 data: [30, 59, 80, 81, 56, 55, 40],
-            },
+            }
         ],
     };
 
@@ -55,30 +63,26 @@ const DesignerDashboard = () => {
         datasets: [
             {
                 label: 'Annotation Types',
-                backgroundColor: 'rgba(255,99,132,0.2)',
-                borderColor: 'rgba(255,99,132,1)',
-                borderWidth: 0,
-                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                hoverBorderColor: 'rgba(255,99,132,1)',
+                backgroundColor: chartColors.background,
+                borderColor: chartColors.borderColor,
+                borderWidth: chartColors.borderWidth,
+                hoverBackgroundColor: chartColors.hoverBackgroundColor,
+                hoverBorderColor: chartColors.hoverBorderColor,
                 data: [59, 80, 56, 55, 40],
             },
         ],
     };
 
     const options = {
-        // tooltips: {
-        //   enabled: false,
-        //   custom: customTooltips
-        // },
-        maintainAspectRatio: false
+        maintainAspectRatio: true
     }
 
     return (
         <BaseContainer>
             <CRow>
-                <CCol sm="6" lg="3">
+                <CCol sm="6" lg="6" xl="3">
                     <CWidgetBrand
-                        color="twitter"
+                        color="primary"
                         rightHeader="20"
                         rightFooter="Today"
                         leftHeader="1.100"
@@ -88,9 +92,9 @@ const DesignerDashboard = () => {
                     </CWidgetBrand>
                 </CCol>
 
-                <CCol sm="6" lg="3">
+                <CCol sm="6" lg="6" xl="3">
                     <CWidgetBrand
-                        color="twitter"
+                        color="primary"
                         rightHeader="21"
                         rightFooter="Today"
                         leftHeader="1.101"
@@ -100,9 +104,9 @@ const DesignerDashboard = () => {
                     </CWidgetBrand>
                 </CCol>
 
-                <CCol sm="6" lg="3">
+                <CCol sm="6" lg="6" xl="3">
                     <CWidgetBrand
-                        color="twitter"
+                        color="primary"
                         rightHeader="22"
                         rightFooter="Today"
                         leftHeader="1.102"
@@ -112,10 +116,10 @@ const DesignerDashboard = () => {
                     </CWidgetBrand>
                 </CCol>
 
-                <CCol sm="6" lg="3">
+                <CCol sm="6" lg="6" xl="3">
                     <CWidgetBrand
-                        color="twitter"
-                        rightHeader="23"
+                        color="primary"
+                        rightHeader="350"
                         rightFooter="Today"
                         leftHeader="1.103"
                         leftFooter="All time"
@@ -126,10 +130,10 @@ const DesignerDashboard = () => {
             </CRow>
             
             <CRow>
-                <CCol sm="6" lg="3">
+                <CCol sm="6" lg="6" xl="3">
                     <CWidgetDropdown
-                        color="gradient-warning"
-                        header="9.823"
+                        color="primary"
+                        header="Ø 9.823"
                         text="Annos / Day"
                         footerSlot={
                             <ChartLineSimple
@@ -138,19 +142,19 @@ const DesignerDashboard = () => {
                                 backgroundColor="rgba(255,255,255,.2)"
                                 dataPoints={[78, 81, 80, 45, 34, 12, 40]}
                                 options={{ elements: { line: { borderWidth: 2.5 } } }}
-                                pointHoverBackgroundColor="warning"
-                                label="Members"
-                                labels="months"
+                                pointHoverBackgroundColor="primary"
+                                label="Annotations"
+                                labels="days"
                             />
                         }
                     >
                     </CWidgetDropdown>
                 </CCol>
 
-                <CCol sm="6" lg="3">
+                <CCol sm="6" lg="6" xl="3">
                     <CWidgetDropdown
-                        color="gradient-warning"
-                        header="500"
+                        color="primary"
+                        header="Ø 500"
                         text="Annotasks / Day"
                         footerSlot={
                             <ChartLineSimple
@@ -159,77 +163,63 @@ const DesignerDashboard = () => {
                                 backgroundColor="rgba(255,255,255,.2)"
                                 dataPoints={[4,0,4]}
                                 options={{ elements: { line: { borderWidth: 2.5 } } }}
-                                pointHoverBackgroundColor="warning"
-                                label="Members"
-                                labels="months"
+                                pointHoverBackgroundColor="primary"
+                                label="Annotasks"
+                                labels="days"
                             />
                         }
                     >
                     </CWidgetDropdown>
                 </CCol>
 
-                <CCol sm="6" lg="3">
+                <CCol sm="6" lg="6" xl="3">
                     <CWidgetDropdown
-                        color="gradient-info"
-                        header="9.823"
-                        text="Members online"
+                        color="primary"
+                        header="Ø 534"
+                        text="Time per Annotation / Day"
                         footerSlot={
                             <ChartLineSimple
-                                pointed
-                                className="mt-3 mx-3"
+                                className="mt-3"
                                 style={{ height: '70px' }}
-                                dataPoints={[1, 18, 9, 17, 34, 22, 11]}
-                                pointHoverBackgroundColor="info"
-                                options={{ elements: { line: { tension: 0.00001 } } }}
-                                label="Members"
-                                labels="months"
+                                backgroundColor="rgba(255,255,255,.2)"
+                                dataPoints={[998,569,332,998,200,569,332]}
+                                options={{ elements: { line: { borderWidth: 2.5 } } }}
+                                pointHoverBackgroundColor="primary"
+                                label="Seconds"
                             />
                         }
                     >
-                        <CDropdown>
+                    </CWidgetDropdown>
+                </CCol>
+
+                <CCol sm="6" lg="6" xl="3">
+                    <CWidgetDropdown
+                        color="primary"
+                        header="Ø 500"
+                        text="Processed Images / Day"
+                        footerSlot={
+                            <ChartLineSimple
+                                className="mt-3"
+                                style={{ height: '70px' }}
+                                backgroundColor="rgba(255,255,255,.2)"
+                                dataPoints={[50,400,300,150,800,600,350]}
+                                options={{ elements: { line: { borderWidth: 2.5 } } }}
+                                pointHoverBackgroundColor="primary"
+                                label="Images"
+                            />
+                        }
+                    >
+                        {/* <CDropdown>
                             <CDropdownToggle caret={false} color="transparent">
                                 <CIcon name="cil-location-pin" />
                             </CDropdownToggle>
-                            <CDropdownMenu className="pt-0" placement="bottom-end">
+                            <CDropdownMenu className="pt-0" placement="bottom-end" backgroundColor="red">
                                 <CDropdownItem>Action</CDropdownItem>
                                 <CDropdownItem>Another action</CDropdownItem>
                                 <CDropdownItem>Something else here...</CDropdownItem>
                                 <CDropdownItem disabled>Disabled action</CDropdownItem>
                             </CDropdownMenu>
-                        </CDropdown>
-                    </CWidgetDropdown>
-                </CCol>
-
-                <CCol sm="6" lg="3">
-                    <CWidgetDropdown
-                        color="gradient-danger"
-                        header="9.823"
-                        text="Members online"
-                        footerSlot={
-                            <ChartBarSimple
-                                className="mt-3 mx-3"
-                                style={{ height: '70px' }}
-                                backgroundColor="rgb(250, 152, 152)"
-                                label="Members"
-                                labels="months"
-                            />
-                        }
-                    >
-                        <CDropdown>
-                            <CDropdownToggle
-                                caret
-                                className="text-white"
-                                color="transparent"
-                            >
-                                <CIcon name="cil-settings" />
-                            </CDropdownToggle>
-                            <CDropdownMenu className="pt-0" placement="bottom-end">
-                                <CDropdownItem>Action</CDropdownItem>
-                                <CDropdownItem>Another action</CDropdownItem>
-                                <CDropdownItem>Something else here...</CDropdownItem>
-                                <CDropdownItem disabled>Disabled action</CDropdownItem>
-                            </CDropdownMenu>
-                        </CDropdown>
+                        </CDropdown> */}
                     </CWidgetDropdown>
                 </CCol>
             </CRow>
