5cb1ff32fa3c33df84b9c1a64cbd9a368ec27e95
Gereon Reus <g.reus@l3bm.com>
2022-04-09 15:20:50 +0200

    added tool tips to script arguments

--- frontend/lost/src/containers/pipeline/globalComponents/modals/ScriptArgumentsTable.js
+++ frontend/lost/src/containers/pipeline/globalComponents/modals/ScriptArgumentsTable.js
@@ -1,20 +1,37 @@
 import React from 'react'
-
+import HelpButton from '../../../../components/HelpButton'
 
 const ArgumentsTable = (props) => {
-    if(props.data){
+    if (props.data) {
         return (
             <div style={{ marginLeft: 15, marginRight: 15 }}>
                 <table className="table table-bordered">
                     <thead>
-                        <tr><th>Key</th><th>Value</th></tr>
+                        <tr>
+                            <th>Key</th>
+                            <th>Value</th>
+                        </tr>
                     </thead>
                     <tbody>
                         {Object.keys(props.data).map((key) => {
                             return (
                                 <tr key={key}>
-                                    <th>{key}</th>
-                                    <td><input onInput={props.onInput} className="form-control" data-ref={key} defaultValue={props.data[key].value} disabled={props.onInput?'': 'disabled'} /></td>
+                                    <th>
+                                        {key}{' '}
+                                        <HelpButton
+                                            id={key}
+                                            text={props.data[key].help}
+                                        />
+                                    </th>
+                                    <td>
+                                        <input
+                                            onInput={props.onInput}
+                                            className="form-control"
+                                            data-ref={key}
+                                            defaultValue={props.data[key].value}
+                                            disabled={props.onInput ? '' : 'disabled'}
+                                        />
+                                    </td>
                                 </tr>
                             )
                         })}
@@ -22,11 +39,9 @@ const ArgumentsTable = (props) => {
                 </table>
             </div>
         )
-    }else {
-        return(<></>)
+    } else {
+        return <></>
     }
-    
 }
-
 
 export default ArgumentsTable
