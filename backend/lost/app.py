a548a73e1ada17ca16c0c6c4a0eea8c36a606cfa
Gereon Reus <g.reus@l3bm.com>
2022-09-23 10:51:20 +0200

    added extra logging for webservices lost-base

--- backend/lost/app.py
+++ backend/lost/app.py
@@ -1,8 +1,10 @@
 import threading
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, g
 from flask_cors import CORS
 from flask import request
 from flask_jwt_extended import JWTManager
+import logging
+import traceback
 from lost import settings
 from lost.api.api import api
 from lost.api.user.endpoint import namespace as user_namespace
@@ -23,6 +25,16 @@ from lost.logic import dask_session
 from lost.db import access
 
 from flaskapp import app, blacklist
+
+
+if settings.LOST_CONFIG.use_graylog:
+    from pygelf import GelfUdpHandler
+    logging.basicConfig(level=logging.INFO)
+    from flask.logging import default_handler
+    app.logger.removeHandler(default_handler)
+    app.logger.addHandler(GelfUdpHandler(host='graylog', port=12201,  _type='lost-api', include_extra_fields=True))
+    app.logger.info('Started LOST Flask Application.')  
+
 
 app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_EXPANSIONS
 app.config['RESTPLUS_VALIDATE'] = settings.RESTPLUS_VAL
@@ -75,6 +87,36 @@ api.add_namespace(config_namespace)
 api.add_namespace(anno_example_namespace)
 app.register_blueprint(blueprint)
 CORS(app)
+
+@app.errorhandler(Exception)
+def handle_500(e):
+    app.logger.error(traceback.format_exc())
+    # original = getattr(e, "original_exception", None)
+    # if original is None:
+    #     app.logger.error(e)
+    # else:
+    #     app.logger.error(original) 
+    return 'error', 500
+
+import time
+
+@app.before_request
+def before_request():
+    g.start = time.time()
+
+@app.after_request
+def after_request(response):
+    diff = time.time() - g.start
+    if ((response.response) and
+        (200 <= response.status_code < 300) and
+        (response.content_type.startswith('text/html'))):
+        response.set_data(response.get_data().replace(
+            b'__EXECUTION_TIME__', bytes(str(diff), 'utf-8')))
+    app.logger.info('Webservice Meta Info', extra={'response_time': diff, 
+                                         'response_code': response.status_code,
+                                         })
+    return response
+
 
 
 def main():
