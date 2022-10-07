9542aee5e3ead18537e610c8cd99e4dca143f5cf
jgoebel <l3bm@jonasgoebel.net>
2022-10-07 10:43:41 +0200

    converted mia cluster to functional component

--- frontend/lost/src/containers/Annotation/MIA/Cluster.js
+++ frontend/lost/src/containers/Annotation/MIA/Cluster.js
@@ -1,36 +1,62 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import MIAImage from './NewMIAImage'
 import actions from '../../../actions'
 
 import './Cluster.scss'
 import { Alert, Button } from 'reactstrap'
-// import { withRouter } from 'react-router-dom'
-import withRouter from '../../../utils/withRouter'
 
-const { getMiaAnnos, getMiaImage, getWorkingOnAnnoTask, getMiaLabel, finishMia } = actions
+const { getMiaAnnos, getWorkingOnAnnoTask, getMiaLabel, finishMia } = actions
 
-class Cluster extends Component {
-    componentDidMount() {
-        this.props.getMiaAnnos(this.props.maxAmount)
-    }
-    componentWillReceiveProps(props) {
-        if (props.workingOnAnnoTask) {
-            const { size, finished } = props.workingOnAnnoTask
+// class Cluster extends Component {
+const Cluster = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const images = useSelector((state) => state.mia.images)
+    const maxAmount = useSelector((state) => state.mia.maxAmount)
+    const zoom = useSelector((state) => state.mia.zoom)
+    const workingOnAnnoTask = useSelector((state) => state.annoTask.workingOnAnnoTask)
+
+    useEffect(() => {
+        getMiaAnnos(maxAmount)
+    }, [])
+
+    // componentWillReceiveProps(props) {
+    //     if (props.workingOnAnnoTask) {
+    //         const { size, finished } = workingOnAnnoTask
+    //         if (size - finished > 0) {
+    //             if (props.images.length === 0) {
+    //                 dispatch(getMiaAnnos(this.props.maxAmount))
+    //                 dispatch(getMiaLabel())
+    //             }
+    //         }
+    //     } else {
+    //         dispatch(getWorkingOnAnnoTask())
+    //     }
+    // }
+
+    useEffect(() => {
+        if (workingOnAnnoTask) {
+            const { size, finished } = workingOnAnnoTask
             if (size - finished > 0) {
-                if (props.images.length === 0) {
-                    this.props.getMiaAnnos(this.props.maxAmount)
-                    this.props.getMiaLabel()
+                if (images.length === 0) {
+                    dispatch(getMiaAnnos(maxAmount))
+                    dispatch(getMiaLabel())
                 }
             }
         } else {
-            this.props.getWorkingOnAnnoTask()
+            dispatch(getWorkingOnAnnoTask())
         }
+    }, [workingOnAnnoTask])
+
+    const dispatchWorkingAnnoTasks = () => {
+        dispatch(getWorkingOnAnnoTask())
     }
 
-    renderFinishTask() {
-        if (this.props.workingOnAnnoTask) {
-            const { size, finished } = this.props.workingOnAnnoTask
+    const renderFinishTask = () => {
+        if (workingOnAnnoTask) {
+            const { size, finished } = workingOnAnnoTask
             if (size - finished === 0) {
                 return (
                     <div style={{ display: 'flex', justifyContent: 'center' }}>
@@ -43,9 +69,11 @@ class Cluster extends Component {
                                 color="primary"
                                 size="lg"
                                 onClick={() =>
-                                    this.props.finishMia(
-                                        this.props.history.push('dashboard'),
-                                        this.props.getWorkingOnAnnoTask,
+                                    dispatch(
+                                        finishMia(
+                                            navigate('/dashboard'),
+                                            dispatchWorkingAnnoTasks,
+                                        )
                                     )
                                 }
                             >
@@ -65,41 +93,25 @@ class Cluster extends Component {
             </div>
         )
     }
-    render() {
-        if (this.props.images.length > 0) {
-            return (
-                <div className="mia-images">
-                    {this.props.images.map((image) => {
-                        return (
-                            <MIAImage
-                                key={image.id}
-                                image={image}
-                                miaKey={image.id}
-                                height={this.props.zoom}
-                            ></MIAImage>
-                        )
-                    })}
-                </div>
-            )
-        } else {
-            return <React.Fragment>{this.renderFinishTask()}</React.Fragment>
-        }
+
+    if (images.length > 0) {
+        return (
+            <div className="mia-images">
+                {images.map((image) => {
+                    return (
+                        <MIAImage
+                            key={image.id}
+                            image={image}
+                            miaKey={image.id}
+                            height={zoom}
+                        ></MIAImage>
+                    )
+                })}
+            </div>
+        )
+    } else {
+        return <React.Fragment>{renderFinishTask()}</React.Fragment>
     }
 }
 
-function mapStateToProps(state) {
-    return {
-        images: state.mia.images,
-        maxAmount: state.mia.maxAmount,
-        zoom: state.mia.zoom,
-        workingOnAnnoTask: state.annoTask.workingOnAnnoTask,
-        proposedLabel: state.mia.proposedLabel,
-    }
-}
-export default connect(mapStateToProps, {
-    getMiaAnnos,
-    getMiaImage,
-    getWorkingOnAnnoTask,
-    getMiaLabel,
-    finishMia,
-})(withRouter(Cluster))
+export default Cluster
