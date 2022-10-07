ba1d2c786aae4298a240022affb1096bf7b467ae
jgoebel <l3bm@jonasgoebel.net>
2022-10-07 10:44:15 +0200

    removed comments, formatting

--- frontend/lost/src/coreui_containers/TheContent.js
+++ frontend/lost/src/coreui_containers/TheContent.js
@@ -2,33 +2,28 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { CContainer, CFade } from '@coreui/react'
 
-// routes config
-// import routes from '../routes'
-
-
 const TheContent = ({ routes }) => (
     <main className="c-main">
         <CContainer fluid>
             <Routes>
-                {routes.map(
-                    (route) =>
-                        route.component && (
-                            <Route
-                                key={route.path}
-                                path={route.path}
-                                exact={route.exact}
-                                name={route.name}
-                                // element={(props) => (
-                                //     <CFade>
-                                //         <route.component {...props} />
-                                //     </CFade>
-                                // )}
-                                element={<CFade>
-                                    <route.component />
-                                </CFade>}
-                            />
-                        ),
-                )}
+                {
+                    routes.map(
+                        (route) =>
+                            route.component && (
+                                <Route
+                                    key={route.path}
+                                    path={route.path}
+                                    exact={route.exact}
+                                    name={route.name}
+                                    element={
+                                        <CFade>
+                                            <route.component />
+                                        </CFade>
+                                    }
+                                />
+                            ),
+                    )
+                }
             </Routes>
         </CContainer>
     </main>
