ce4a5cb72ed34d47b5b0af1522e4eb8fef35f9a3
jgoebel <l3bm@jonasgoebel.net>
2022-11-04 13:30:04 +0100

    [ongoing] hookifying selectlabel

--- frontend/lost/src/containers/pipeline/start/2/modals/types/annoTaskModal/4/SelectLabel.js
+++ frontend/lost/src/containers/pipeline/start/2/modals/types/annoTaskModal/4/SelectLabel.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Graph from 'react-graph-vis'
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import actions from '../../../../../../../../actions/pipeline/pipelineStartModals/annoTask'
 import HelpButton from '../../../../../../../../components/HelpButton'
 import { CRow } from '@coreui/react'
 import _ from 'lodash'
-
-const { updateLabels } = actions
 
 const options = {
     height: '600px',
@@ -57,78 +55,111 @@ const options = {
     },
 }
 
-class SelectLabel extends Component {
-    constructor() {
-        super()
-        this.events = {
-            select: (event) => {
-                if (this.graphData.isLeafArr.includes(event.nodes[0])) {
-                    // leaf item clicked
-                    this.selectionHandler()
-                    return
+const graphData = {
+    nodes: [],
+    edges: [],
+    isLeafArr: [],
+}
+
+let tree
+let selectedArr
+let selectChildrenArr = []
+
+const SelectLabel = ({ availableLabelTrees, peN, verifyTab }) => {
+
+    const { updateLabels } = actions
+    const stateElement = useSelector((element) => element)
+    const element = stateElement.pipelineStart.step1Data.elements.filter(
+        (el) => el.peN === peN,
+    )[0]
+
+    const selectedLabelTree = element.exportData.annoTask.selectedLabelTree
+    const labelLeaves = element.exportData.annoTask.labelLeaves
+
+    // const [events, setEvents] = useState(null)
+    // const [tree, setTree] = useState(null)
+    // const [selectedArr, setSelectedArr] = useState(null)
+    // const [selectChildrenArr, setSelectChildrenArr] = useState([])
+    // const [graph, setGraph] = useState(React.createRef())
+    // const [graphData, setGraphData] = useState({
+    //     nodes: [],
+    //     edges: [],
+    //     isLeafArr: [],
+    // })
+
+    const graphRef = useRef()
+
+    const events = {
+        select: (event) => {
+            if (graphData.isLeafArr.includes(event.nodes[0])) {
+                // leaf item clicked
+                selectionHandler()
+                return
+            }
+            const arr = event.nodes.map((el) => {
+                return {
+                    id: el,
+                    maxLabels: '3',
                 }
-                const arr = event.nodes.map((el) => {
-                    return {
-                        id: el,
-                        maxLabels: '3',
-                    }
-                })
-                const isDoublicated =
-                    this.props.labelLeaves.filter((el) => el.id === event.nodes[0])
-                        .length > 0
-                let editedArr
-                if (isDoublicated) {
-                    // deselect --> filter array
-                    editedArr = this.props.labelLeaves.filter(
-                        (el) => !(el.id === event.nodes[0]),
-                    )
-                } else {
-                    // select --> merge arrays
-                    editedArr = _.unionBy(arr, this.props.labelLeaves, 'id')
-                }
-                this.props.updateLabels(this.props.peN, editedArr)
-                this.props.verifyTab(this.props.peN, 3, true)
-                this.selectionHandler()
-            },
-        }
-        this.graph = React.createRef()
+            })
+            const isDuplicated =
+                labelLeaves.filter((el) => el.id === event.nodes[0]).length > 0
+
+            let editedArr
+
+            if (isDuplicated) {
+                // deselect --> filter array
+                editedArr = labelLeaves.filter(
+                    (el) => !(el.id === event.nodes[0]),
+                )
+            } else {
+                // select --> merge arrays
+                editedArr = _.unionBy(arr, labelLeaves, 'id')
+            }
+
+            updateLabels(peN, editedArr)
+            verifyTab(peN, 3, true)
+            selectionHandler()
+        },
     }
 
-    selectionHandler() {
-        this.selectedArr = this.props.labelLeaves.map((el) => el.id)
-        this.selectChildrenArr = []
-        const parentIsInList =
-            this.selectedArr.filter((el) => el === this.tree.idx).length > 0
-        if (parentIsInList) {
-            this.selectChildrenArr = this.tree.children.map((el) => el.idx)
-        }
-        this.findChildren(this.tree)
-        this.graph.current.Network.selectNodes(this.selectChildrenArr)
-    }
-
-    findChildren(branch) {
+    const findChildren = (branch) => {
         branch.children.forEach((el) => {
-            const isInList = this.selectedArr.filter((el2) => el2 === el.idx).length > 0
+            const isInList = selectedArr.filter((el2) => el2 === el.idx).length > 0
             if (isInList) {
-                this.selectChildrenArr = [
-                    ...this.selectChildrenArr,
+                selectChildrenArr = [
+                    ...selectChildrenArr,
                     ...el.children.map((el3) => el3.idx),
                 ]
+                // setSelectChildrenArr([
+                //     ...selectChildrenArr,
+                //     ...el.children.map((el3) => el3.idx),
+                // ])
             }
-            if (el.children.length) {
-                this.findChildren(el)
-            }
+
+            if (el.children.length) findChildren(el)
         })
     }
 
-    componentDidMount() {
-        this.selectionHandler()
+    // useEffect(() => {
+
+    // }, [selectedArr, tree])
+
+    // useEffect(() => {
+    //     if (graphData === null) return
+
+    //     mapTreeToGraph(tree, tree.idx)
+    // }, [graphData])
+
+    const selectionHandler = () => {
+        // setSelectedArr(labelLeaves.map((el) => el.id))
+        selectedArr = labelLeaves.map((el) => el.id)
     }
 
-    mapTreeToGraph(branch, parent) {
+    const mapTreeToGraph = (graphData, branch, parent) => {
         branch.children.forEach((el) => {
             if (parent) {
-                this.graphData.edges.push({
+                graphData.edges.push({
                     from: parent,
                     to: el.idx,
                 })
@@ -140,66 +171,127 @@ class SelectLabel extends Component {
                 font: { color: '#FFFFFF' },
             }
             if (el.children.length) {
-                this.mapTreeToGraph(el, el.idx)
+                mapTreeToGraph(graphData, el, el.idx)
             } else {
-                this.graphData.isLeafArr.push(el.idx)
+                graphData.isLeafArr.push(el.idx)
             }
-            this.graphData.nodes.push(nodeObj)
+            graphData.nodes.push(nodeObj)
         })
     }
 
-    render() {
-        this.graphData = {
-            nodes: [],
-            edges: [],
-            isLeafArr: [],
-        }
-        this.tree = this.props.availableLabelTrees.filter(
-            (el) => el.idx === this.props.selectedLabelTree,
+    useEffect(() => {
+        // graph = React.createRef()
+
+        // const _events = {
+        //     select: (event) => {
+        //         if (graphData.isLeafArr.includes(event.nodes[0])) {
+        //             // leaf item clicked
+        //             selectionHandler()
+        //             return
+        //         }
+        //         const arr = event.nodes.map((el) => {
+        //             return {
+        //                 id: el,
+        //                 maxLabels: '3',
+        //             }
+        //         })
+        //         const isDuplicated =
+        //             labelLeaves.filter((el) => el.id === event.nodes[0]).length > 0
+
+        //         let editedArr
+
+        //         if (isDuplicated) {
+        //             // deselect --> filter array
+        //             editedArr = labelLeaves.filter(
+        //                 (el) => !(el.id === event.nodes[0]),
+        //             )
+        //         } else {
+        //             // select --> merge arrays
+        //             editedArr = _.unionBy(arr, labelLeaves, 'id')
+        //         }
+
+        //         updateLabels(peN, editedArr)
+        //         verifyTab(peN, 3, true)
+        //         selectionHandler()
+        //     },
+        // }
+
+        // setEvents(_events)
+
+        selectionHandler()
+
+        const _tree = availableLabelTrees.filter(
+            (el) => el.idx === selectedLabelTree,
         )[0]
-        this.graphData.nodes.push({
-            id: this.tree.idx,
-            label: this.tree.name,
+        // setTree(_tree)
+        tree = _tree
+
+        // if (selectedArr === null || tree === null) return
+
+        const parentIsInList = selectedArr.filter((el) => el === tree.idx).length > 0
+        if (parentIsInList) {
+            console.log("Parent in list");
+            selectChildrenArr = tree.children.map((el) => el.idx)
+        }
+
+        findChildren(tree)
+        console.log({ selectChildrenArr });
+        graphRef.current.Network.selectNodes(selectChildrenArr)
+
+        // const _graphData = graphData
+        graphData.nodes.push({
+            id: tree.idx,
+            label: tree.name,
             chosen: true,
-            color: this.tree.color ? this.tree.color : '#10515F',
+            color: tree.color ? tree.color : '#10515F',
             font: { color: '#FFFFFF' },
         })
 
-        this.mapTreeToGraph(this.tree, this.tree.idx)
 
-        return (
-            // <Card className="annotask-modal-card">
-            //     <CardBody>
-            <>
-                <CRow className="justify-content-center">
-                    <HelpButton
-                        id={'choose-label'}
-                        text={`Click on the parent label to make all child labels available in the AnnotationTask. 
-                        Multiple parent labels can also be selected. 
-                        Labels that are active for the AnnotationTask are visualized with a strong border in this view.`}
-                    />
-                </CRow>
-                <Graph
-                    ref={this.graph}
-                    graph={this.graphData}
-                    options={options}
-                    events={this.events}
+        console.info(graphData);
+
+
+        // setGraphData(_graphData)
+
+        mapTreeToGraph(graphData, tree, tree.idx)
+    }, [])
+
+    // if (graphData === null || events === null) return 'Loading...'
+
+    return (
+        // <Card className="annotask-modal-card">
+        //     <CardBody>
+        <>
+            <CRow className="justify-content-center">
+                <HelpButton
+                    id={'choose-label'}
+                    text={`Click on the parent label to make all child labels available in the AnnotationTask. 
+                    Multiple parent labels can also be selected. 
+                    Labels that are active for the AnnotationTask are visualized with a strong border in this view.`}
                 />
-            </>
-            //     </CardBody>
-            // </Card>
-        )
-    }
+            </CRow>
+            <Graph
+                ref={graphRef}
+                graph={graphData}
+                options={options}
+                events={events}
+            />
+        </>
+        //     </CardBody>
+        // </Card>
+    )
 }
 
-const mapStateToProps = (state, test) => {
-    const element = state.pipelineStart.step1Data.elements.filter(
-        (el) => el.peN === test.peN,
-    )[0]
+// const mapStateToProps = (state, test) => {
+//     const element = state.pipelineStart.step1Data.elements.filter(
+//         (el) => el.peN === test.peN,
+//     )[0]
 
-    return {
-        selectedLabelTree: element.exportData.annoTask.selectedLabelTree,
-        labelLeaves: element.exportData.annoTask.labelLeaves,
-    }
-}
-export default connect(mapStateToProps, { updateLabels })(SelectLabel)
+//     return {
+//         selectedLabelTree: element.exportData.annoTask.selectedLabelTree,
+//         labelLeaves: element.exportData.annoTask.labelLeaves,
+//     }
+// }
+
+// export default connect(mapStateToProps, { updateLabels })(SelectLabel)
+export default SelectLabel
