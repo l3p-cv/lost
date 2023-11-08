a09e3d94eec16f78c1dd6f1544eea5290c0f1c6e
Jonas Göbel <j.goebel@l3bm.com>
2023-11-08 16:38:13 +0100

    remove edit button for annotasks

--- frontend/lost/src/containers/Datasets/DatasetTable.jsx
+++ frontend/lost/src/containers/Datasets/DatasetTable.jsx
@@ -10,10 +10,10 @@ import {
 } from '@tanstack/react-table'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { CTable, CTableBody, CTableHead } from '@coreui/react'
-import { faCaretDown, faCaretRight, faDownload, faEye, faFile } from '@fortawesome/free-solid-svg-icons'
+import { faCaretDown, faCaretRight, faDownload, faEye, faFile, faPen } from '@fortawesome/free-solid-svg-icons'
 import IconButton from '../../components/IconButton'
 
-const DatasetTable = ({ datasetList, datastores, onExportButtonClicked }) => {
+const DatasetTable = ({ datasetList, datastores, onExportButtonClicked, onEditButtonClicked }) => {
     const [tableData, setTableData] = React.useState(() => [...datasetList])
 
     // update the table when the parameter data changes
@@ -31,20 +31,20 @@ const DatasetTable = ({ datasetList, datastores, onExportButtonClicked }) => {
     const renderRowIcon = (row) => {
 
         // show file icon for annotasks
-        if (row.depth.isAnnotask) {
+        if (row.original.is_annotask) {
             // also use icon to indent child row (margin)
-            return <FontAwesomeIcon size="xl" icon={faFile} style={{ marginLeft: row.depth * 20 }} />
+            return <FontAwesomeIcon size="xl" icon={faFile} style={{ marginLeft: row.depth * 25 }} />
         }
 
         // dataset that can be opened
         if (row.getCanExpand()) {
             // also use icon to indent child row (margin)
-            return <FontAwesomeIcon size="2xl" icon={row.getIsExpanded() ? faCaretDown : faCaretRight} onClick={() => row.toggleExpanded()} style={{ marginLeft: row.depth * 20 }} />
+            return <FontAwesomeIcon size="2xl" icon={row.getIsExpanded() ? faCaretDown : faCaretRight} onClick={() => row.toggleExpanded()} style={{ marginLeft: row.depth * 25 }} />
         }
 
         // dataset that cannot be openened (it has no annotasks assigned)
         // also use icon to indent child row (margin)
-        return <FontAwesomeIcon size="2xl" icon={faCaretRight} color="grey" style={{ marginLeft: row.depth * 20 }} />
+        return <FontAwesomeIcon size="2xl" icon={faCaretRight} color="grey" style={{ marginLeft: row.depth * 25 }} />
     }
 
     const columns = [
@@ -74,8 +74,8 @@ const DatasetTable = ({ datasetList, datastores, onExportButtonClicked }) => {
             header: () => 'Created at',
         }),
         columnHelper.display({
-            id: 'showAnnotasks',
-            header: () => 'Show Annotasks',
+            id: 'review',
+            header: () => 'Review',
             cell: props => (<IconButton
                 icon={faEye}
                 color="primary"
@@ -85,7 +85,7 @@ const DatasetTable = ({ datasetList, datastores, onExportButtonClicked }) => {
                     showAnnotasks(datasetID)
                 }}
                 disabled={false}
-                text="Review"
+            // text="Review"
             />)
         }),
         columnHelper.display({
@@ -97,13 +97,37 @@ const DatasetTable = ({ datasetList, datastores, onExportButtonClicked }) => {
                     color="primary"
                     isOutline={false}
                     onClick={() => {
-                        const datasetID = props.row.original.id
-                        onExportButtonClicked(datasetID)
+                        const datasetID = props.row.original.idx
+                        const isAnnotask = props.row.original.is_annotask === true
+                        onExportButtonClicked(datasetID, isAnnotask)
                     }}
                     disabled={false}
-                    text="Export"
+                // text="Export"
                 />
             )
+        }),
+        columnHelper.display({
+            id: 'showEdit',
+            header: () => 'Edit',
+            cell: props => {
+                const rowData = props.row.original
+
+                // only show edit icon for datasets
+                if (rowData.is_annotask) return ""
+
+                return (
+                    <IconButton
+                        icon={faPen}
+                        color="primary"
+                        isOutline={false}
+                        onClick={() => {
+                            onEditButtonClicked(props.row.original)
+                        }}
+                        disabled={false}
+                    // text="Edit"
+                    />
+                )
+            }
         })
     ]
 
