6c1af98b3a4b84fbac4602c5c932e66bd5164065
Keshav Upadhyaya <k.upadhyaya@l3bm.com>
2025-01-24 15:20:38 +0100

    update eslint config to prevent conflict with prettier

--- frontend/lost/eslint.config.js
+++ frontend/lost/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
     { ignores: ['dist'] },
@@ -35,4 +36,5 @@ export default [
             ],
         },
     },
+    eslintConfigPrettier,
 ]
