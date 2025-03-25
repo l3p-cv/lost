34f32af0b2a045f8acf8e64261b49c5d84d3f8e4
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2025-03-25 12:21:54 +0100

    remove old files

--- frontend/lost/src/components/Notification.js
+++ frontend/lost/src/components/Notification.js
@@ -1,5 +1,5 @@
 import Swal from 'sweetalert2'
-import * as REQUEST_STATUS from '../types/requestStatus'
+
 const Toast = Swal.mixin({
     toast: true,
     position: 'top-end',
@@ -53,26 +53,6 @@ export const showInfo = (text) => {
 
 export const close = () => {
     Toast.close()
-}
-
-export const handling = (status, successText) => {
-    if (REQUEST_STATUS.LOADING === status) {
-        showLoading()
-    } else if (REQUEST_STATUS.SUCCESS === status) {
-        showSuccess(successText)
-    }
-}
-
-export const networkRequest = (requestStatus) => {
-    if (requestStatus.status) {
-        if (REQUEST_STATUS.LOADING === requestStatus.status) {
-            showLoading()
-        } else if (REQUEST_STATUS.SUCCESS === requestStatus.status) {
-            showSuccess(requestStatus.message)
-        } else if (REQUEST_STATUS.FAILED === requestStatus.status) {
-            showError(requestStatus.message)
-        }
-    }
 }
 
 export const showDecision = ({
--- frontend/lost/src/index.jsx
+++ frontend/lost/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './App'
+import './index.css'
 import reportWebVitals from './reportWebVitals'
 // import 'semantic-ui-css/semantic.min.css'
 
