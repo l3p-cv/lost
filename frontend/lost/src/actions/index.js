b3a459a8ab23f40835876c9ca33fb4eea88905f8
Jörg Groß <j.gross@l3bm.com>
2026-01-15 09:22:57 +0100

    eradicate old mia-api

--- frontend/lost/src/actions/index.js
+++ frontend/lost/src/actions/index.js
@@ -1,11 +1,9 @@
 import annoTask from './annoTask'
-import mia from './mia'
 import sia from './sia'
 import siaReview from './siaReview'
 
 export default {
   ...annoTask,
-  ...mia,
   ...sia,
   ...siaReview,
 }
--- frontend/lost/src/actions/mia/index.js
+++
@@ -1,3 +0,0 @@
-import * as mia from './mia'
-
-export default { ...mia }
