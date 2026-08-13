4beb288cb08eeaf9219b7606cb905876372d5152
divine-dumpling <ridhi19p@gmail.com>
2026-08-13 10:09:18 +0200

    corrected the joyride step comments

--- frontend/lost/vite.config.ts
+++ frontend/lost/vite.config.ts
@@ -2,7 +2,15 @@ import react from '@vitejs/plugin-react'
 import { polyfillNode } from 'esbuild-plugin-polyfill-node'
 import { defineConfig } from 'vite'
 import commonjs from 'vite-plugin-commonjs'
+import path from 'path'
 export default defineConfig({
+  resolve: {
+    alias: {
+      'lost-sia/models': path.resolve(__dirname, '../../../lost-sia/src/models/index.ts'),
+      'lost-sia/utils': path.resolve(__dirname, '../../../lost-sia/src/utils/index.ts'),
+      'lost-sia': path.resolve(__dirname, '../../../lost-sia/src/index.ts'),
+    },
+  },
   plugins: [react(), polyfillNode({ polyfills: { buffer: true } }), commonjs()],
   optimizeDeps: {
     // added this to fix the issue with semantic-ui-react some other dependencies
