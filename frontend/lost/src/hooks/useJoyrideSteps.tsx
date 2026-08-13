4b877794bebb234a3f1a49e59a576b6abcd2de98
divine-dumpling <ridhi19p@gmail.com>
2026-08-13 10:43:56 +0200

    Revert "corrected the joyride step comments"
    This reverts commit 4beb288cb08eeaf9219b7606cb905876372d5152.

--- frontend/lost/src/hooks/useJoyrideSteps.tsx
+++ frontend/lost/src/hooks/useJoyrideSteps.tsx
@@ -650,7 +650,7 @@ export const useJoyrideSteps = (
         spotlightClicks: true,
       },
       {
-        // 9
+        // 8
         target: '.last-row-highlight',
         title: 'New Instruction Row',
         content: 'This is your new instruction row.',
@@ -658,7 +658,7 @@ export const useJoyrideSteps = (
         spotlightClicks: false,
       },
       {
-        // 10
+        // 9
         target: '.edit-instruction-button',
         title: 'Edit Instruction',
         content: 'Click to edit your new instruction.',
@@ -666,7 +666,7 @@ export const useJoyrideSteps = (
         spotlightClicks: true,
       },
       {
-        // 11
+        // 10
         target: '.edit-instructions-modal',
         content:
           'Here you can edit the instruction details. Make changes and save when done.',
@@ -675,7 +675,7 @@ export const useJoyrideSteps = (
         spotlightClicks: true,
       },
       {
-        // 12
+        // 11
         target: '.save-button',
         title: 'Save Instruction',
         content: 'Click to save your instruction.',
@@ -683,7 +683,7 @@ export const useJoyrideSteps = (
         spotlightClicks: true,
       },
       {
-        // 13
+        // 12
         target: '#nav-start-pipeline',
         title: 'Start Pipelines Tab',
         content:
@@ -693,7 +693,7 @@ export const useJoyrideSteps = (
         spotlightClicks: false,
       },
       {
-        // 14
+        // 13
         target: '.pipeline-start-1',
         title: 'Pipeline Templates',
         content: 'These are the available pipeline templates to start your SIA pipeline.',
@@ -701,7 +701,7 @@ export const useJoyrideSteps = (
         spotlightClicks: false,
       },
       {
-        // 15
+        // 14
         target: '.sia-start-button',
         title: 'Start SIA Pipeline',
         content: 'Click this button to open the SIA pipeline template.',
@@ -1078,7 +1078,7 @@ export const useJoyrideSteps = (
         spotlightClicks: true,
       },
       {
-        // 9
+        // 8
         target: '.last-row-highlight',
         title: 'New Instruction Row',
         content: 'This is your new instruction row.',
@@ -1086,7 +1086,7 @@ export const useJoyrideSteps = (
         spotlightClicks: false,
       },
       {
-        // 10
+        // 9
         target: '.edit-instruction-button',
         title: 'Edit Instruction',
         content: 'Click to edit your new instruction.',
@@ -1094,7 +1094,7 @@ export const useJoyrideSteps = (
         spotlightClicks: true,
       },
       {
-        // 11
+        // 10
         target: '.edit-instructions-modal',
         content:
           'Here you can edit the instruction details. Make changes and save when done.',
@@ -1103,7 +1103,7 @@ export const useJoyrideSteps = (
         spotlightClicks: true,
       },
       {
-        // 12
+        // 11
         target: '.save-button',
         title: 'Save Instruction',
         content: 'Click to save your instruction.',
@@ -1111,7 +1111,7 @@ export const useJoyrideSteps = (
         spotlightClicks: true,
       },
       {
-        // 13
+        // 12
         target: '#nav-pipelines',
         title: 'Pipelines Tab',
         content: 'Click here to navigate to the Pipelines page to start an SIA pipeline.',
@@ -1120,7 +1120,7 @@ export const useJoyrideSteps = (
         spotlightClicks: false,
       },
       {
-        // 14
+        // 13
         target: '.latest-pipeline-open-button',
         title: 'Open Latest Pipeline',
         content: 'Click this button to view the latest created pipeline.',
@@ -1129,7 +1129,7 @@ export const useJoyrideSteps = (
         disableBeacon: true,
       },
       {
-        // 15
+        // 14
         target: '.react-flow__node.react-flow__node-annoTaskNode',
         title: 'Edit Instructions',
         content: 'To edit instructions in your pipeline, click next.',
@@ -1138,7 +1138,7 @@ export const useJoyrideSteps = (
         spotlightClicks: true,
       },
       {
-        // 16
+        // 15
         target: '.inactive-tab-class',
         title: 'Switch Tab',
         content: 'Click this tab to switch to the final instruction step.',
@@ -1147,7 +1147,7 @@ export const useJoyrideSteps = (
         disableBeacon: true,
       },
       {
-        // 17
+        // 16
         target: '.instruction-tab',
         title: 'Instruction Options Tab',
         content:
